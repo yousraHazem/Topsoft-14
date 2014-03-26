f41209b0b56ddb7af892fe4af377a60dff9e2c3c
Shary <shary@ubuntu.(none)>
2014-03-26 19:35:07 +0000

    verifying_child_account

--- kidzen/app/controllers/child_controller.rb
+++ kidzen/app/controllers/child_controller.rb
@@ -1,10 +1,17 @@
 # Authors: Ahmed H. Ismail
 class ChildController < ApplicationController
 
+    def new
+        UserMailer.account_verification.deliver #after creating the sign up we will pass the email
+    end
 
     def create_friendship(child_1, child_2)
         FriendShip.create(child_1_id: child_1.id, child_2_id: child_2.id)
     end
 
+    def verify
+        @child.is_approved = true
+    end 
     
+
 end
