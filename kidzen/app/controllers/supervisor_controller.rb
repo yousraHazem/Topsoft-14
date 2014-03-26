bdd7a493d693f2aac2570bc2751aae4d7e5989ed
Radwan PC <radwan@ubuntu.ubuntu-domain>
2014-03-26 13:46:51 -0700

    Added Supervisor skeleton 3

---
+++ kidzen/app/controllers/supervisor_controller.rb
@@ -0,0 +1,20 @@
+class SupervisorController < ApplicationController
+def addSuperVisor # add a supervisor
+end
+
+def removeSuperVisor  # remove a supervisor
+end
+
+def addChild # add a child
+end
+
+def moveChild #move a child
+end
+
+def moveChild #move a child
+end
+
+def exchangeLegalG #exchange the legal gaurdian
+end
+
+end
---
+++ kidzen/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kidzen::Application.config.secret_key_base = '78fda554cbc866ccfb8393e8bbbea5be5e5979171a18db3353468dddcb8f59f880950743261e1d21477569afd3a21125f241f73cb844dca7115fdc5332bfd011'
