473884185e61743dcf473b678dea00dbe1a58021
mohamedkhaledabdel <mohamed.kh.abdelmeguid@gmail.com>
2014-03-27 20:18:56 +0200

    conv

--- kidzen/app/models/parent_child.rb
+++ kidzen/app/models/parent_child.rb
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
 class ParentChild < ActiveRecord::Base
-=======
 # Represents ParentOf relationship
 #Authors: Ahmed H. Ismail
 class ParentChild < ActiveRecord::Base
   belongs_to :child, foreign_key: 'child_id'
   belongs_to :supervisor, foreign_key: 'parent_id'
->>>>>>> 6220626cf07ed4ce26538d770a38dc9e0b17a97e
 end
