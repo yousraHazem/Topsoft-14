480acf80042ed462537c3ae9cc1a5fb1c837f1f2
Ali Ahmed <alloka@alloka.(none)>
2014-03-27 11:26:13 +0200

    added

---
+++ kidzen/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kidzen::Application.config.secret_key_base = '0c2a5c97751db720a0f1c271d943d9494ca72ef44aed3c850d9aa0c5686e8cac9887fc619cc541d67cfb3982b7b459bd1dee2b9fed215ef4a52cca9bd929dc3b'
