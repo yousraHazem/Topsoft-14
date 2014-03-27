919b7ae09b1c44c0d089e5b3c451290a409066bc
sharybeshara <sharybeshara@gmail.com>
2014-03-27 15:38:54 +0000

    Update profiles_controller.rb

--- kidzen/app/controllers/profiles_controller.rb
+++ kidzen/app/controllers/profiles_controller.rb
@@ -3,7 +3,7 @@ class ProfilesController < ApplicationController
  @super = Supervisor.first
  #.first has to be changed using signIn/Login
  @parent = ParentChild.find_by parent_name: @super.name
- if @parent.parent_name == @super.name && @parent.child_name != nil
- redirect_to access_access_path
+ if (@parent.parent_name == @super.name && @parent.child_name != nil)
+   redirect_to access_access_path
  end
 end
