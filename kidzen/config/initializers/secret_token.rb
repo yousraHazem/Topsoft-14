20062ecedacdcfd380866d474107bcc1db4e51b9
nouranMamdouhAbdo <nouran.mamdouh.33@gmail.com>
2014-03-27 15:48:14 +0200

    created event_tag

---
+++ kidzen/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kidzen::Application.config.secret_key_base = '4a0762a5708aec43c31d5bf07e9af57ea5c21d357c058fc2f1d506c69d62e356cb965dae8b2aa7ce14467d430c45175e990575beb206e0e935ce29e7f6c5f262'
