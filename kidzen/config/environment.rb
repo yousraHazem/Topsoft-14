d7ece718f75917569e3b985ff2eae42fead3e4bd
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2014-03-28 15:56:01 +0200

    Fixed conflicts in config/enviroment.rb

--- kidzen/config/environment.rb
+++ kidzen/config/environment.rb
@@ -2,9 +2,5 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
-<<<<<<< HEAD
-
-=======
->>>>>>> 62d85970fc5313f1786eaa33541c3fd359f911c5
 Kidzen::Application.initialize!
 
