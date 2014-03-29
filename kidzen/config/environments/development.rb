24d05531d5496555cffeb13e5b681b950d6f60a1
mohamedkhaledabdel <mohamed.kh.abdelmeguid@gmail.com>
2014-03-29 05:33:25 +0200

    ..

--- kidzen/config/environments/development.rb
+++ kidzen/config/environments/development.rb
@@ -28,10 +28,7 @@ Kidzen::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   :authentication => :plain,
@@ -42,14 +39,10 @@ Kidzen::Application.configure do
   :password => "kidZen14"
 }
 
->>>>>>> 6220626cf07ed4ce26538d770a38dc9e0b17a97e
-=======
-=======
-=======
+
 
  
->>>>>>> 62d85970fc5313f1786eaa33541c3fd359f911c5
->>>>>>> development
+
 
   # This configration to allow the user_mailer send emails
   # Authors: Shary Beshara
@@ -62,12 +55,5 @@ Kidzen::Application.configure do
     :user_name => "kidzennetwork",
     :password => "kidZen14"
   }
-<<<<<<< HEAD
->>>>>>> development
-=======
-<<<<<<< HEAD
-=======
 
->>>>>>> 62d85970fc5313f1786eaa33541c3fd359f911c5
->>>>>>> development
 end
--- kidzen/config/initializers/secret_token.rb
+++ kidzen/config/initializers/secret_token.rb
@@ -9,27 +9,9 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 Kidzen::Application.config.secret_key_base = '44c10a97ff86d1188da10ab235abbcb0c485cce3d776b126956a4fd419a25734e16d9a5ad456e42a8b712a31384ea6657379340379cce85e1f00ebd01ad8f14d'
-=======
 Kidzen::Application.config.secret_key_base = 'd66453cad6645b34c9e91ebc6831914deb4092627eda1404234ecbc25eba134c9fb4e88781e8f5f769a9f30a5a02bb452a38604661c9d39f2580725b703f6fe7'
->>>>>>> 6220626cf07ed4ce26538d770a38dc9e0b17a97e
-=======
-=======
-=======
-
->>>>>>> 62d85970fc5313f1786eaa33541c3fd359f911c5
->>>>>>> development
-
 Kidzen::Application.config.secret_key_base = 'd66453cad6645b34c9e91ebc6831914deb4092627eda1404234ecbc25eba134c9fb4e88781e8f5f769a9f30a5a02bb452a38604661c9d39f2580725b703f6fe7'
 
-<<<<<<< HEAD
->>>>>>> development
-=======
-<<<<<<< HEAD
 
-=======
->>>>>>> 62d85970fc5313f1786eaa33541c3fd359f911c5
->>>>>>> development
