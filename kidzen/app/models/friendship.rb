e3b4c4f6a60548afa7fdc8ef74d9a57dc9a1681e
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2014-04-11 13:34:30 +0200

    Creates friendships table

--- kidzen/app/models/friendship.rb
+++ kidzen/app/models/friendship.rb
@@ -2,7 +2,7 @@
 # The FriendShip relation.
 # Between two children
 # Authors: Ahmed H. Ismail
-class FriendShip < ActiveRecord::Base
+class Friendship < ActiveRecord::Base
   # Associations
   belongs_to :child, foreign_key: 'child_1_id'
   belongs_to :child, foreign_key: 'child_2_id'
