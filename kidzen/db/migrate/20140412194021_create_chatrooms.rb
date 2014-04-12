f3ca98b2ff3c226d02ba2c765c1b6dcaa1aaa0a7
Nour2610 <nour.tarek2610@gmail.com>
2014-04-12 23:00:47 +0200

    Modified create_chatrooms migration

--- kidzen/db/migrate/20140412194021_create_chatrooms.rb
+++ kidzen/db/migrate/20140412194021_create_chatrooms.rb
@@ -2,7 +2,7 @@ class CreateChatrooms < ActiveRecord::Migration
   def change
     create_table :chatrooms do |t|
     	t.string :chatroom_name
-    	t.string :created_by_username
+    	t.string :creator_username
     	t.boolean :invite_only
     	t.boolean :mutual_only
     	
