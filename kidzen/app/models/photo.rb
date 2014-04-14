13fe5a5bd69abd0360fb25f942f00c14f37d76fa
hussieneloy <hussien.eloy@gmail.com>
2014-04-14 10:50:25 +0200

    Issue #57 Add validations

--- kidzen/app/models/photo.rb
+++ kidzen/app/models/photo.rb
@@ -1,7 +1,9 @@
 class Photo < ActiveRecord::Base
 	mount_uploader :image, ImageUploader
 	# has_many :comments,:tags    <<< to be un-commented once these models are created
-	# belongs_to :post    <<< to be un-commented once these models are created           
+	# belongs_to :post    <<< to be un-commented once these models are created        
+	validates_presence_of :description
+	validate :local_or_remote   
 
 	def photo_params
 		params.require(:photo).permit(:description, :image, :remote_image_url)
@@ -31,6 +33,12 @@ class Photo < ActiveRecord::Base
 
 	def edit_description(description)
 		self[:description] = description
+	end
+	private
+	def local_or_remote
+		if !(image.blank? ^ remote_image_url.blank?)
+			errors.add(:base, "Please Add photo")
+		end
 	end
 end
 
