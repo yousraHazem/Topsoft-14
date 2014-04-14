6dbdb10feb320f458fa8ecaaa1a3b7c581854d1c
hussieneloy <hussien.eloy@gmail.com>
2014-04-14 08:18:12 +0200

    Issue #57 Add Model Relations and stubs

--- kidzen/app/models/photo.rb
+++ kidzen/app/models/photo.rb
@@ -1,5 +1,7 @@
 class Photo < ActiveRecord::Base
 	mount_uploader :image, ImageUploader
+	# has_many :comments,:tags    <<< to be un-commented once these models are created
+	# belongs_to :post    <<< to be un-commented once these models are created           
 
 	def photo_params
 		params.require(:photo).permit(:description, :image, :remote_image_url)
@@ -11,6 +13,20 @@ class Photo < ActiveRecord::Base
 
 	def get_description
 		self[:description]
+	end
+
+	def get_comments
+		# self.comments    <<< to be un-commented once these models are created
+	end
+
+	def get_tags
+		# self.tags    <<< to be un-commented once these models are created
+	end
+
+	def write_comment(comment)
+	end
+
+	def tag_someone(registered_user)
 	end
 
 	def edit_description(description)
