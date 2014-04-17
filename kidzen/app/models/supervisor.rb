171c865f3bfc2476282668620667cec21f0da1d6
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2014-04-17 05:13:15 +0200

    Creates bare bone supervisor model #138

--- kidzen/app/models/supervisor.rb
+++ kidzen/app/models/supervisor.rb
@@ -1,5 +1,16 @@
+# Supervisor model.
+# Authors: Ahmed H. Ismail
 class Supervisor < ActiveRecord::Base
     has_one :registered_user, dependent: :destroy
+    has_many :children, through: :child_parent
+    has_many :supervised_children, through: :child_supervisor, class_name: "Child"
     validates_associated :registered_user
     self.primary_key = 'registered_user_id'
+
+    # Returns an array of children pending verification.
+    # Gets them through the ChildParent relationship.
+    # Authors: Ahmed H. Ismail
+    def pending_children
+      children.select { | c | !c.approved? }
+    end
 end
