97c685ed5df5fdd54d75b4d99b0d2e36c83f5e58
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2014-04-17 05:16:52 +0200

    Initial implementation of supervisors#confirm_children action #138

--- kidzen/app/controllers/supervisors_controller.rb
+++ kidzen/app/controllers/supervisors_controller.rb
@@ -1,4 +1,8 @@
 class SupervisorsController < ApplicationController
+  
+  # GET /confirm_children
+  # Authors: Ahmed H. Ismail
   def confirm_children
+    @children = @supervisor.pending_children
   end
 end
