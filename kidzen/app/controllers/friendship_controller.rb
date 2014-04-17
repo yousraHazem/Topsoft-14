97d75b1937217f850cc1f1c68a3457950453e0b1
khaled-barca <khaled.elhossiny10@gmail.com>
2014-04-18 01:55:20 +0200

    almost done with send_friend_request action

--- kidzen/app/controllers/friendship_controller.rb
+++ kidzen/app/controllers/friendship_controller.rb
@@ -6,13 +6,21 @@ class FriendshipController < ApplicationController
  end
  
  def view_my_friends
- @friends=current_user.friends
+ @friends = current_user.friends
  end
 
  def view_pending_friendship_requests
  end
  
  def send_friend_request
+ @user_id  =  current_user.id
+ @friendship1  =  Friendship.new(:user_id => params[:friend_id].to_i, :friend_id => @user_id, :status => "pending")
+ @friendship2=Friendship.new(:user_id => @user_id, :friend_id => params[:friend_id].to_i, :status => "requested")
+ if @friendship1.save && @friendship2.save
+ flash[:notice]  =  "friend request sent"
+ else flash[:error]  =  "unable to send friend request"
+ end
+ #render('find_friends')
  end
 
  def accept_reject_friend_request
