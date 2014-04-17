7da6bf91a8dc490197043bf74d0f2c6d793c022c
hussieneloy <hussien.eloy@gmail.com>
2014-04-17 10:02:09 +0200

    Issue #151 Add video controller with basic views

---
+++ kidzen/app/controllers/videos_controller.rb
@@ -0,0 +1,48 @@
+class VideosController < ApplicationController
+  def video_params
+  params.require(:video).permit(:description, :length, :category)
+  end
+
+  def index
+    @videos = Video.all
+  end
+
+  def new
+    @video = Video.new
+  end
+
+  def show
+    @video = Video.find(params[:id])
+  end
+
+  def create
+    @video = Video.new(video_params)
+    if @video.save
+      flash[:notice] = "Successfully created video"
+      redirect_to @video
+    else
+      render :action => 'new'
+    end
+  end
+
+  def edit
+    @video = Video.find(params[:id])
+  end
+
+  def update
+    @video = Video.find(params[:id])
+    if @video.update_attributes(video_params)
+      flash[:notice] = "Successfully updated video"
+      redirect_to video_url
+    else
+    render :action => 'edit' 
+    end
+  end
+
+  def destroy
+    @video = Video.find(params[:id])
+    @video.destroy
+    flash[:notice] = "Successfully destroyed video"
+    redirect_to videos_url
+  end
+end
