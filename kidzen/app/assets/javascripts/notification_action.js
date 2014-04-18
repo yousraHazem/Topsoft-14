d09b0ffdc93f64201fe2e2f6f5c6f6e475f4a8a3
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2014-04-19 01:03:12 +0200

    Implements notification_action_click event handler #152

--- kidzen/app/assets/javascripts/notification_action.js
+++ kidzen/app/assets/javascripts/notification_action.js
@@ -1,4 +1,3 @@
-
 /************************************************ 
  * Handles notification actions on click event. *
  * Performs a put request.                      *
@@ -6,6 +5,10 @@
  * notificationID - pretty obvious              *
  * Authors: Ahmed H. Ismail.                    *
  ************************************************/
-function notification_action_click (actionURL, notificationID) {
-  
+function notification_action_click (actionURL, notificationID, data) {
+  // note that we are not interested in response
+  var xhr = new XMLHttpRequest();
+  xhr.open('PUT', [location.hostname, actionURL].join('/'), true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.send( JSON.stringify(data) );
 }
