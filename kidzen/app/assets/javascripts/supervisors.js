37beef378acb7d6e4210be26ecf1b84ca016b9ec
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2014-04-18 19:22:53 +0200

    Creats superisors.js and moves generic functions #152

---
+++ kidzen/app/assets/javascripts/supervisors.js
@@ -0,0 +1,26 @@
+/**
+ * @brief  Handles collapsing and expanding fields.
+ * @authors Ahmed H. Ismail
+ * @param  element, this element when clicked.
+ * @return void
+ */
+function collapse (element) {
+  var checkBox = element.firstElementChild.firstElementChild;
+  if(checkBox != null) {
+    checkBox.checked = !checkBox.checked; // toggle
+    var collapsableDiv = element.firstElementChild.nextElementSibling;
+    if(collapsableDiv != null) {
+      if(checkBox.checked) {
+        // make visible
+        collapsableDiv.style.visibility="visible";
+      }else {
+        collapsableDiv.style.visibility="hidden";
+      }
+    }else {
+      console.log("Error in collapse function could not find collapsableDiv");
+    }
+    console.log(checkBox);
+  }else {
+    console.log("Error in collapse function could not find checkBox");
+  }
+}
--- kidzen/app/assets/javascripts/supervisors_confirm_children.js
+++ kidzen/app/assets/javascripts/supervisors_confirm_children.js
@@ -1,31 +1,3 @@
-/**
- * @brief  Handles collapsing and expanding fields.
- * @authors Ahmed H. Ismail
- * @param  element, this element when clicked.
- * @return void
- */
-function collapse (element) {
-  var checkBox = element.firstElementChild.firstElementChild;
-  if(checkBox != null) {
-    checkBox.checked = !checkBox.checked; // toggle
-    var collapsableDiv = element.firstElementChild.nextElementSibling;
-    if(collapsableDiv != null) {
-      if(checkBox.checked) {
-        // make visible
-        collapsableDiv.style.visibility="visible";
-      }else {
-        collapsableDiv.style.visibility="hidden";
-      }
-    }else {
-      console.log("Error in collapse function could not find collapsableDiv");
-    }
-    console.log(checkBox);
-  }else {
-    console.log("Error in collapse function could not find checkBox");
-  }
-}
-
-
 /****************************************************
  * Creates an AJAX request.                         *
  * Always uses a put request.                       *
