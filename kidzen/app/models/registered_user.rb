cb496058140cdbc15ac74bfc6892439aa227b890
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2014-04-19 01:12:50 +0200

    Moves child created notification to Supervisor model #152

--- kidzen/app/models/registered_user.rb
+++ kidzen/app/models/registered_user.rb
@@ -84,37 +84,7 @@ class RegisteredUser < ActiveRecord::Base
     def ban
     end
 
-    # Adds a notification that a child was created.
-    # creates notification and saves to database.
-    # child - child entity that was created.
-    # returns true if successful otherwise false
-    def notify_child_created(child)
-      notification = Notification.new
-      # Set attributes
-      notification.mark_unread
-      notification.title = "New child #{child.full_name}"
-      notification.short_desc = "A new child needs approval."
-      notification.long_desc = ""
-      notification.embedded_view_url = nil
-      # Create actions
-      actions = notification.add_child_creation_actions
-      actions_saved = true
-      actions.each do |action| 
-        actions_saved &= action.save
-      end
-      # Enqueue
-      queue_notification( notification )
-      if notification.save and actions_saved
-        true
-      else
-        # Clean up 
-        actions.each do |action| 
-          action.destroy
-        end
-        notification.destroy
-        false
-      end
-    end
+
 
     # Queues a notification as pending for this user.
     # Namely adds the foreign key.
