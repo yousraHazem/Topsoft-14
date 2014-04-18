8f50b6a60402fa82e5edbfcc3040258d08d628cb
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2014-04-18 17:02:12 +0200

    Waiting for signin/signup story #138

--- kidzen/app/controllers/supervisors_controller.rb
+++ kidzen/app/controllers/supervisors_controller.rb
@@ -1,8 +1,55 @@
+# Supervisor controller
+# Authors: Ahmed H. Ismail
 class SupervisorsController < ApplicationController
-  
+  before_action :grab_pending_child_by_name, only: [:accept_child, :reject_child]
+  before_action :set_supervisor, except: [] # set for all
+  wrap_paramaters format: [:json], include: [:child_username]
   # GET /confirm_children
+  # Renders the confirm children
+  # view.
   # Authors: Ahmed H. Ismail
   def confirm_children
     @children = @supervisor.pending_children
   end
+
+
+  # PUT /supervisor/reject_child
+  # Accept child action.
+  # Authors: Ahmed H. Ismail
+  def accept_child
+    respond_to do |format| 
+      msg =  {status: "ok"}
+      format.json { render json: msg }
+      format.html { redirect_to: :confirm_children }
+    end
+  end
+
+  # PUT /supervisor/accept_child
+  # Reject child action.
+  # Authors: Ahmed H. Ismail
+  def reject_child
+    respond_to do |format|
+      msg = {status: "ok"}
+      format.json { render json: msg }
+      format.html { redirect_to: :confirm_children }
+    end
+  end
+
+  private
+
+  # Safety first.
+  # Authors Ahmed H. Ismail
+  def supervisor_params
+    params.require(:supervisor).permit(:user_name)
+  end
+
+  # Sets supervisor instance var to 
+  # the currently logged in supervisor or nil
+  # if none.
+  # Authors: Ahmed H. Ismail
+  def set_supervisor
+    #TODO: implement
+  end
+
+
 end
