a17486bc59f53f4a778bf36c1d8c47d5a7c03ebc
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2014-04-18 18:40:58 +0200

    Adds default values for notifications model #152

--- kidzen/app/models/notification.rb
+++ kidzen/app/models/notification.rb
@@ -6,4 +6,11 @@ class Notification < ActiveRecord::Base
   validates :long_desc, presence: true
   has_many :notification_actions, dependent: :destroy
   belongs_to :registered_user, foreign_key: 'assigned_to', primary_key: 'username'
+  before_save :default_values
+
+  # Sets default values
+  # Authors: Ahmed H. Ismail
+  def default_values
+    self[:pending] = true
+  end
 end
