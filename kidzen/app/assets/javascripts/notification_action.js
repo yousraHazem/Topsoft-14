2a7b0a147684e97220d8d15cba8d89b9dbdc85bb
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2014-04-18 23:48:04 +0200

    Adds js and jsinclude for notification action logic #152

---
+++ kidzen/app/assets/javascripts/notification_action.js
@@ -0,0 +1,11 @@
+
+/************************************************ 
+ * Handles notification actions on click event. *
+ * Performs a put request.                      *
+ * actionURL -  path to action                  *
+ * notificationID - pretty obvious              *
+ * Authors: Ahmed H. Ismail.                    *
+ ************************************************/
+function notification_action_click (actionURL, notificationID) {
+  
+}
