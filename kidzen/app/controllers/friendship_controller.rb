41f977190b26cccd20c5d69e900f5bcc51ee2198
khaled-barca <khaled.elhossiny10@gmail.com>
2014-04-18 19:36:39 +0200

    done with implementing 'accept_reject_friendship_requests' action

--- kidzen/app/controllers/friendship_controller.rb
+++ kidzen/app/controllers/friendship_controller.rb
@@ -6,15 +6,24 @@ class FriendshipController < ApplicationController
  end
  
  def view_my_friends
- @friends=current_user.friends
  end
 
  def view_pending_friendship_requests
+ @pending_friends=current_user.pending_friends
  end
  
  def send_friend_request
  end
 
  def accept_reject_friend_request
+ if params[:status].to_i == 1
+ Friendship.where("friend_id=#{current_user.id} AND user_id=#{params[:friend_id]}").update_all(status: "accepted")
+ Friendship.where("friend_id=#{params[:friend_id]} AND user_id=#{current_user.id}").update_all(status: "accepted")
+ flash[:notice] = "friend request successfully accepted"
+ else
+ @destroyed_friendship=Friendship.delete_all("friend_id=#{current_user.id} AND user_id=#{params[:friend_id]}")
+ flash[:notice] = "friend request successfully rejected"
+ end
+ redirect_to(:controller => 'friendship', :action => 'view_pending_friendship_requests')
  end
 end
