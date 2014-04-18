7e8ec350ae0b3e6be535745e6bcb048d168ae22b
moazelnashar <moazelnashar@gmail.com>
2014-04-18 12:37:43 +0200

    modifying attributes in table photos

--- kidzen/db/migrate/20140417130546_photos.rb
+++ kidzen/db/migrate/20140417130546_photos.rb
@@ -2,7 +2,7 @@ class Photos < ActiveRecord::Migration
   def change
     create_table :photos do |t|
       t.string :description
-      t.string :name
+      t.string :image
       t.string :remote_image_url
       t.timestamps
     end
