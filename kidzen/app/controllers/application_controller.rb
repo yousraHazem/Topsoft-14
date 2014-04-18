9a52cec5037f2419a83446b0366697a70606007c
ammarwa <ammarwa@gmail.com>
2014-04-18 16:43:36 +0200

    knowing current logged in redited

--- kidzen/app/controllers/application_controller.rb
+++ kidzen/app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_devise_permitted_parameters, if: :devise_controller?
+  session[:current_user_email] = :email
 
   protected
 
