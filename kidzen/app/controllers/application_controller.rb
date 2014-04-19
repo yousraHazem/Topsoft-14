8c7a1c6f6cd830e97b2e769cbc5b3ac1a73e14e2
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2014-04-19 21:11:59 +0200

    Problem with gender field in signup

--- kidzen/app/controllers/application_controller.rb
+++ kidzen/app/controllers/application_controller.rb
@@ -21,15 +21,12 @@ class ApplicationController < ActionController::Base
     def configure_devise_permitted_parameters
       registration_params = [:first_name, :family_name, :username, :nickname, :middle_name, :banned, :gender, :birth_date, :email, :password, :password_confirmation, :is_child]
 
-      if params[:action] == 'update'
-        devise_parameter_sanitizer.for(:account_update) { 
-          |u| u.permit(registration_params << :current_password)
-      }
-      elsif params[:action] == 'create'
-        devise_parameter_sanitizer.for(:sign_up) { 
-          |u| u.permit(registration_params) 
-        }
-      end
+      devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(registration_params)}
+
+    
+      devise_parameter_sanitizer.for(:account_update) { 
+        |u| u.permit(registration_params << :current_password) }
     end
+      
 
 end
