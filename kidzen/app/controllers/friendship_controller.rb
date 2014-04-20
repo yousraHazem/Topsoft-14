20a52f0952778df2dc5d4649c1b5b1cd86b78c31
khaled-barca <khaled.elhossiny10@gmail.com>
2014-04-20 22:20:03 +0200

    added routes and matched the variables with the migrations

--- kidzen/app/controllers/friendship_controller.rb
+++ kidzen/app/controllers/friendship_controller.rb
@@ -1,8 +1,7 @@
 class FriendshipController < ApplicationController
+
  def index
- end
- 
- def find_friends
+
  end
  
  def view_my_friends
@@ -10,19 +9,22 @@ class FriendshipController < ApplicationController
  end
 
  def view_pending_friendship_requests
+
  end
  
  def send_friend_request
  @user_id  =  current_user.id
- @friendship1  =  Friendship.new(:user_id => params[:friend_id].to_i, :friend_id => @user_id, :status => "pending")
- @friendship2=Friendship.new(:user_id => @user_id, :friend_id => params[:friend_id].to_i, :status => "requested")
+ @friendship1  =  Friendship.new(:child1_id => params[:friend_id].to_i, :child2_id => @user_id, :status => "pending")
+ @friendship2  =  Friendship.new(:child1_id => @user_id, :child2_id => params[:friend_id].to_i, :status => "requested")
  if @friendship1.save && @friendship2.save
  flash[:notice]  =  "friend request sent"
- else flash[:error]  =  "unable to send friend request"
+ else 
+ flash[:error]  =  "unable to send friend request"
  end
- #render('find_friends')
  end
 
  def accept_reject_friend_request
+
  end
+
 end
--- kidzen/config/routes.rb
+++ kidzen/config/routes.rb
@@ -15,7 +15,7 @@ Kidzen::Application.routes.draw do
   resources :groups
   get "child/verify"
   post "child/new"
-  get "friendship/view_my_friends"
+  get "friendship/send_friend_request"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
