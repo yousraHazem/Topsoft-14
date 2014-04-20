c41dcdcb25104a2cc83b4506634c921446105bbf
moazelnashar <moazelnashar@gmail.com>
2014-04-20 21:28:35 +0200

    C2_S11_As_a_user_I_can_search_for_other_users,_events,_groups_and_activities #178

--- kidzen/app/models/event.rb
+++ kidzen/app/models/event.rb
@@ -1,7 +1,3 @@
 class Event < ActiveRecord::Base
-	def self.search(search)
-  search_condition = "%" + search + "%"
-  find(:all, :conditions => ['title LIKE ? OR description LIKE ?', search_condition, search_condition])
-end
-
+	searchkick autocomplete: [:name]	
 end
