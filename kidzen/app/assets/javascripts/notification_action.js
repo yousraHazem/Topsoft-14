3b8d24dc6fcc632717544ee8692f78492d69db89
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2014-04-21 01:01:46 +0200

    Fixes url in notification_action onclic #152

--- kidzen/app/assets/javascripts/notification_action.js
+++ kidzen/app/assets/javascripts/notification_action.js
@@ -8,7 +8,7 @@
 function notification_action_click (actionURL, notificationID, data) {
   // note that we are not interested in response
   var xhr = new XMLHttpRequest();
-  xhr.open('PUT', [location.hostname, actionURL].join('/'), true);
+  xhr.open('PUT', ['http://' + location.host , actionURL].join('/'), true);
   xhr.setRequestHeader('Content-Type', 'application/json');
   xhr.send( JSON.stringify(data) );
 }
