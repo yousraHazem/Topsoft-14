f14a27ebd3e6e75026f0e1e48fe6f8c082323e10
khaled-barca <khaled.elhossiny10@gmail.com>
2014-04-20 22:35:41 +0200

    matched variables with the migrations

--- kidzen/app/controllers/friendship_controller.rb
+++ kidzen/app/controllers/friendship_controller.rb
@@ -1,11 +1,11 @@
 class FriendshipController < ApplicationController
+
  def index
+
  end
- 
- def find_friends
- end
- 
+  
  def view_my_friends
+
  end
 
  def view_pending_friendship_requests
@@ -13,17 +13,19 @@ class FriendshipController < ApplicationController
  end
  
  def send_friend_request
+
  end
 
  def accept_reject_friend_request
- if params[:status].to_i == 1
- Friendship.where("friend_id=#{current_user.id} AND user_id=#{params[:friend_id]}").update_all(status: "accepted")
- Friendship.where("friend_id=#{params[:friend_id]} AND user_id=#{current_user.id}").update_all(status: "accepted")
- flash[:notice] = "friend request successfully accepted"
+ if params[:status].to_i==1
+ Friendship.where("child1_id=#{current_user.id} AND child2_id=#{params[:friend_id]}").update_all(status:"accepted")
+ Friendship.where("child1_id=#{params[:friend_id]} AND child2_id=#{current_user.id}").update_all(status:"accepted")
+ flash[:notice]="friend request successfully accepted"
  else
- @destroyed_friendship=Friendship.delete_all("friend_id=#{current_user.id} AND user_id=#{params[:friend_id]}")
- flash[:notice] = "friend request successfully rejected"
+ @destroyed_friendship=Friendship.delete_all("child1_id=#{current_user.id} AND child2_id=#{params[:friend_id]}")
+ flash[:notice]="friend request successfully rejected"
  end
- redirect_to(:controller => 'friendship', :action => 'view_pending_friendship_requests')
+ redirect_to(:controller => 'friendship',:action => 'view_pending_friendship_requests')
  end
+
 end
