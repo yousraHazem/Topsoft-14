fa071bb208dbabc19f1cb602bd625a0d880ae136
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2014-04-20 19:06:25 +0200

    Adds login view

--- kidzen/app/controllers/sessions_controller.rb
+++ kidzen/app/controllers/sessions_controller.rb
@@ -1,4 +1,23 @@
 # Controller for user Sessions
 # Authors: Ahmed H. Ismail
 class SessionsController < ApplicationController
+
+  # GET /signin
+  # Renders signin form
+  # Authors: Ahmed H. Ismail
+  def new
+  end
+
+  # POST /sessions
+  # Creates a new session
+  # Authors: Ahmed H. Ismail
+  def create
+  end
+
+  # DELETE /signout
+  # Logs out a user
+  # Authors: Ahmed H. Ismail
+  def destroy
+  end
+
 end
