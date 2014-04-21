1180ff00bc04ca4e301a30e8d62c70cf9518ee19
AbdelrahmanSaad <sonic_goku@hotmail.com>
2014-04-21 14:42:50 +0200

    Issue #139 , Adding Documentation

--- kidzen/test/controllers/calendar_controller_test.rb
+++ kidzen/test/controllers/calendar_controller_test.rb
@@ -1,5 +1,5 @@
 require 'test_helper'
-
+#This tests show of the Calendar and show wether it works or not
 class CalendarControllerTest < ActionController::TestCase
   test "should get show" do
     get :show
