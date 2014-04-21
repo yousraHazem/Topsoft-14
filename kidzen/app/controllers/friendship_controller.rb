ae4c5bfda41fdca642fb2f0ca6b0e8d72289bb30
khaled-barca <khaled.elhossiny10@gmail.com>
2014-04-21 22:33:18 +0200

    added documentations

--- kidzen/app/controllers/friendship_controller.rb
+++ kidzen/app/controllers/friendship_controller.rb
@@ -11,16 +11,26 @@ class FriendshipController < ApplicationController
  def view_pending_friendship_requests
 
  end
- 
+
+ # This method to send a friendship request.
+ # user_id - the id of the child currently signed in.
+ # friendship1 - active record of the pending friendship relation.
+ # friendship2 - active record of the requested friendship relation.
+ # the method then saves the 2 records in the friendship table.
+ # it then redirects to the action and controller that invoked this action.
+ # Time complexity: O(n).
+ #Authors: Khaled I. Elhossiny.
+
  def send_friend_request
- @user_id  =  current_user.id
- @friendship1  =  Friendship.new(:child1_id => params[:friend_id].to_i, :child2_id => @user_id, :status => "pending")
- @friendship2  =  Friendship.new(:child1_id => @user_id, :child2_id => params[:friend_id].to_i, :status => "requested")
- if @friendship1.save && @friendship2.save
+ user_id  =  current_user.id
+ friendship1  =  Friendship.new(:child1_id => params[:friend_id].to_i, :child2_id => user_id, :status => "pending")
+ friendship2  =  Friendship.new(:child1_id => user_id, :child2_id => params[:friend_id].to_i, :status => "requested")
+ if friendship1.save && friendship2.save
  flash[:notice]  =  "friend request sent"
  else 
  flash[:error]  =  "unable to send friend request"
  end
+ redirect_to(:controller => params[:cont],:action => params[:act])
  end
 
  def accept_reject_friend_request
