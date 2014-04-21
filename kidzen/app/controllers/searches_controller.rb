8fecae5ec186dfeb0b6713899ef4e39fd36cf9f7
moazelnashar <moazelnashar@gmail.com>
2014-04-21 13:24:22 +0200

    C2_S11_As_a_user_I_can_search_for_other_users,_events,_groups_and_activities #178

---
+++ kidzen/app/controllers/searches_controller.rb
@@ -0,0 +1,15 @@
+class SearchesController < ApplicationController
+
+# This is a function which take a parameter ":search" and search with it in each table
+# Returns all found results in each table.
+# Time complexity: 4*O(n).
+#Authors: Moaz El-Nashar.	
+	def index
+		if params[:search].present?
+			@registered_users = RegisteredUser.search params[:search]
+			@events = Event.search params[:search]
+			@groups = Group.search params[:search]
+			@activities = Activity.search params[:search]
+		end
+	end	
+end
