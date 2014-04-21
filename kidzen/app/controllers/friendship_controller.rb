e3ef6d1065bbd2551ca624d25a2038cce49ff83a
khaled-barca <khaled.elhossiny10@gmail.com>
2014-04-21 22:44:18 +0200

    added documentations and organized the associations

--- kidzen/app/controllers/friendship_controller.rb
+++ kidzen/app/controllers/friendship_controller.rb
@@ -3,8 +3,16 @@ class FriendshipController < ApplicationController
  def index
  end
  
+ # This method to provide the child with a list of friends.
+ # child - the child currently signed in.
+ # @friends - active record of children.
+ # Returns @friends which is the active record of the child's friends.
+ # Time complexity: O(n).
+ #Authors: Khaled I. Elhossiny.
+ 
  def view_my_friends
- @friends = current_user.friends
+ child=Child.where("registered_user_id=#{current_user.id}").first
+ @friends = child.friends
  end
 
  def view_pending_friendship_requests
--- kidzen/app/models/friendship.rb
+++ kidzen/app/models/friendship.rb
@@ -1,4 +1,5 @@
 class Friendship < ActiveRecord::Base
+# Associations
 belongs_to :child, :class_name => 'User', :foreign_key =>'child1_id'
 belongs_to :friend, :class_name => 'User', :foreign_key =>'child2_id'
 end
