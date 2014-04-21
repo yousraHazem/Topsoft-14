f3372804a83321acebf58e80cab82ea2613c1646
nouranMamdouhAbdo <nouran.mamdouh.33@gmail.com>
2014-04-21 23:05:12 +0200

    add route fior view friends

--- kidzen/config/routes.rb
+++ kidzen/config/routes.rb
@@ -15,6 +15,7 @@ Kidzen::Application.routes.draw do
   resources :groups
   get "child/verify"
   post "child/new"
+  get "event/view_friends"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
