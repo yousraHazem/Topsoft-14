43af6c0ea9a7b51a0d13baacf28cbb1db2c04bca
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2014-04-23 00:52:33 +0200

    Documents RegisteredUsersController

--- kidzen/app/controllers/registered_users_controller.rb
+++ kidzen/app/controllers/registered_users_controller.rb
@@ -1,24 +1,28 @@
+# Controller for RegisteredUsers
+# Authors: Ahmed H. Ismail
 class RegisteredUsersController < ApplicationController
  
- def show 
 
+ # GET /registered_user
+ # GET /profile
+ # Redirects to the profile page of the currently
+ # logged in user. 
+ # Authors: Ahmed H. Ismail
+ def show 
     if !signed_in? 
       # Login page
       # TODO: Change to guest home page when/if created
-      redirect_to session_path :new
-    
+      redirect_to session_path :new    
     else
       # Someone is signed in
       if Supervisor.exists?( registered_user: current_user)
-        # Supervisor
+        # Redirect to Supervisor
         redirect_to controller: :supervisors, action: :show
       else 
-        # Child
+        # Redirect to Child
         redirect_to controller: :children, action: :show
       end
-
     end
-
   end
 
 end
