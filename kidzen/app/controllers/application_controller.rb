6c7edc5c454a4c63a1db278d0e3db5bb9faa92b8
moazelnashar <moazelnashar@gmail.com>
2014-04-22 16:18:47 +0200

    fixing some doc

--- kidzen/app/controllers/application_controller.rb
+++ kidzen/app/controllers/application_controller.rb
@@ -4,13 +4,20 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :check_locale
 
+  # This is function changes the language of the system from Ar to En or vice versa.
+  # Time complexity: O(1).
+  #Authors: Moaz El-Nashar.
   def change_locale
   	session[:locale]= params[:locale]
   	redirect_to :back, notice: "Your language has been changes to #{params[:locale]}"
   end
 
+  # This is function check the current language of the system.
+  # Time complexity: O(1).
+  #Authors: Moaz El-Nashar.
   private 
   def check_locale
     I18n.locale = session[:locale] || :en
   end
+  
 end
