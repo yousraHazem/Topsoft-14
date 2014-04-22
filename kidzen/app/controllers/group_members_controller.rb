7319f55af9be6257a33f9cf55382d443e2ffd098
mbelraka <mohamed.elrakayby@gmail.com>
2014-04-22 02:10:03 +0200

    Uploading first version of the code

---
+++ kidzen/app/controllers/group_members_controller.rb
@@ -0,0 +1,25 @@
+class GroupMembersController < ApplicationController
+    
+
+    
+    # This piece of code allows a user to view the members of a group
+    # The members are populated in @group_members to allow the model to generate a list of members
+    # Returns nothing
+    # Time Complexity: O(n).
+    # Author: Mohamed Bahgat Elrakaiby
+    def index
+      @group_members = GroupMember.all
+    end
+
+    # This is a function that allows a user to leave a group
+    # It fetches the record from the database then deletes it
+    # Returns nothing
+    # Time complexity: O(1).
+    # Author: Mohamed Bahgat Elrakaiby
+    def leave_group
+      m = GroupMember.where(:username => current_user.username , :group_id => :group_id)
+      m.destroy
+      @g = Group.where(:group_id => :group_id)
+      redirect_to @g
+    end
+end 
