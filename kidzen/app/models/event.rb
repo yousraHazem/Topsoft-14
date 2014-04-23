42cae88b277175c540c269fec30471a8b858afc6
nouranMamdouhAbdo <nouran.mamdouh.33@gmail.com>
2014-04-23 13:14:41 +0200

    date time validations

--- kidzen/app/models/event.rb
+++ kidzen/app/models/event.rb
@@ -1,4 +1,23 @@
 class Event < ActiveRecord::Base
-    #validation
-	validates_presence_of :name , :location
+#validation
+#Author: Nouran Mamdouh
+validates_presence_of :name , :location
+#COMMMEEENNNNTTT
+attr_accessor :month, :day, :year
+  validate :validate_created_at
+
+private
+
+def convert_created_at
+  begin
+    self.created_at = Date.civil(self.year.to_i, self.month.to_i, self.day.to_i)
+  rescue ArgumentError
+    false
+  end
+end
+
+def validate_created_at
+  errors.add("Created at date", "is invalid.") unless convert_created_at
+end
+
 end
