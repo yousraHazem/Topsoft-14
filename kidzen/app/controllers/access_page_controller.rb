ccd0bd5c24d1e30b5917e9c991420a42060ae8e9
mohamedkhaledabdel <mohamed.kh.abdelmeguid@gmail.com>
2014-04-24 13:27:47 +0200

    done with the conventions and the db corrections

--- kidzen/app/controllers/access_page_controller.rb
+++ kidzen/app/controllers/access_page_controller.rb
@@ -1,48 +1,50 @@
 ## The access page controller
 ##Author:- Mohamed khaled abdelmeguid
 
-## change DB tables and attributes to be compatible with the master
+## change DB tables and attributes to be compatible with the master (DONE)
 
 class AccessPageController < ApplicationController
-  #respond_to :html, :json
   def access
-  	## check if the children of a parent is not empty
-    if Child.find(:all).empty?
-  	  puts("error")
-  	end
-  	  @child = Child.all
+  	# check if the children of a parent is not empty
+    if RegisteredUser.find_by(id: ChildParent.find_by(parent_id: current_user.id).child_id).nil?
+    &&ChildParent.find_by(parent_id: current_user.id).nil?
+    && ChildParent.find_by(parent_id: current_user.id).child_id.nil?
+    &&Supervisor.find_by(supervisor_id: current_user.id).nil?
+      puts("error there is no child")
+    else
+  	  @child = ChildParent.find_all_by_parent_id (current_user.id)
+    end
+
   		## here we take the values to be updated from the url but there is a bug in updating the Hash
-  		#check = params[:c1]
-  		#child = params[:childUpdate]
-    	#@update = Permit.find_by(child_name: child).update(abilities[:can_add_mutual_friends] => check)
-    #if @update.save
-      #redirect_to root_url
-    #else
-      #flash[:notice_update_has_failed] = true
-      #edirect_to root_url
-    #end
+    check = params[:c1]
+  	child = params[:childUpdate]
+    @update = Permission.find_by(child_name: child).update(abilities[:can_add_mutual_friends] => check)
+    if @update.save
+      redirect_to root_url
+    else
+      flash[:notice_update_has_failed] = true
+      edirect_to root_url
+    end
   	
   	## here we take the values to be updated from the url but there is a bug in updating the DB 
   		
-  	#text = params[:written]
-  	#child = params[:childUpdate]
-    #@upText = Permit.find_by(child_name: child).update(tags => check)
-	
-	##if condition to search for the tag if found we put an error else we add it to the database
-	##esle if statment
-    ## if Permission.find_by(current_user.username).tags()	
-    #if @update.save
-     #redirect_to root_url
-    #else
-     #flash[:notice_update_has_failed] = true
-     #redirect_to root_url
-    #end
+  	text = params[:written]
+  	child = params[:childUserName]
+    if Permission.find_by(registered_user_id: RegisteredUser.find_by(username: child).id).tags == text
+      puts("this tag is already submitted, please enter a new tag that is not found in the list")
+    end
+    @upText = Permission.find_by(registered_user_id: RegisteredUser.find_by(username: child).id).update(tags => text)
+
+    if @update.save
+      redirect_to root_url
+    else
+      flash[:notice_update_has_failed] = true
+      redirect_to root_url
+    end
   
+    end
   end
-
-
- 
-  end
+  
 
 
 
