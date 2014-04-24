e515a4050d43e720a9c038851672244f68444d64
nouranMamdouhAbdo <nouran.mamdouh.33@gmail.com>
2014-04-24 23:23:10 +0200

    renaming child id

---
+++ kidzen/db/migrate/20140424134205_fix_friendship.rb
@@ -0,0 +1,6 @@
+class FixFriendship < ActiveRecord::Migration
+  def change
+  	rename_column("friendships","child_1_id","child1_id")
+  	rename_column("friendships","child_2_id","child2_id")
+  end
+end
