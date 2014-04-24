65fb3bf06ed78b37ee7d1360d8aca2f0de786010
moazelnashar <moazelnashar@gmail.com>
2014-04-24 11:49:47 +0200

    adding migrations

---
+++ kidzen/db/migrate/20140420002645_create_poll_question.rb
@@ -0,0 +1,10 @@
+class CreatePollQuestion < ActiveRecord::Migration
+  def change
+    create_table :poll_questions do |t|
+      t.string :content
+      t.datetime :created_at
+      t.datetime :updated_at
+      t.timestamps
+    end      
+  end
+end
