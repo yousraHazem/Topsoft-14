d5f347060700e13614307c5e29c859566ffbbf0d
khaled-barca <khaled.elhossiny10@gmail.com>
2014-04-25 20:14:36 +0200

    removed migrations

--- kidzen/db/migrate/20140411135146_create_permission.rb
+++
@@ -1,9 +0,0 @@
-class CreatePermission < ActiveRecord::Migration
-  def change
-    create_table :permissions do |t|
-        t.text :abilities
-        t.integer :registered_user_id
-        t.timestamps
-    end
-  end
-end
