d0c28e8d7973ce5f55b0a9353bc4fc876129b54f
madostar <medu2013@gmail.com>
2014-04-25 16:26:32 +0200

    Update group_members_controller.rb

--- kidzen/app/controllers/group_members_controller.rb
+++ kidzen/app/controllers/group_members_controller.rb
@@ -1,14 +1,14 @@
 class GroupMembersController < ApplicationController
-	layout false
+    layout false
 
-	def index
-	  @group_members = GroupMember.all
-	end
+    def index
+      @group_members = GroupMember.all
+    end
 
-	# This is a function that adds a member to the group by his name.
-	# user is a variable of type RegisteredUser.
-	# child is a variable of type Child.
-	# member is a variavle of type GroupMember.
+    # This is a function that adds a member to the group by his name.
+    # user is a variable of type RegisteredUser.
+    # child is a variable of type Child.
+    # member is a variavle of type GroupMember.
     # Time complexity: O(1).
     # Authors: Mohammed T. Nabih.
     def add_member_name
@@ -28,8 +28,8 @@ class GroupMembersController < ApplicationController
     # Time complexity: O(n).
     # Authors: Mohammed T. Nabih.
     def add_member_email
-   	  user = RegisteredUser.all.where(:email => :email)
-   	  child = Child.where(:registered_user_id => user.id)
+      user = RegisteredUser.all.where(:email => :email)
+      child = Child.where(:registered_user_id => user.id)
       member = GroupMember.where(:username => user.username , :group_id => :group_id)
       member.member_accept_state = 2
       member.save
@@ -44,7 +44,7 @@ class GroupMembersController < ApplicationController
     # Authors: Mohammed T. Nabih.
     def add_member_phone
       user = RegisteredUser.all.where(:phone => :phone)
-   	  child = Child.where(:registered_user_id => user.id)
+      child = Child.where(:registered_user_id => user.id)
       member = GroupMember.where(:username => user.username , :group_id => :group_id)
       member.member_accept_state = 2
       memberd.save
