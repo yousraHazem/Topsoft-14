66d3e3a0028b751ed6be3d7849ffcdf4d969ede7
khaled-barca <khaled.elhossiny10@gmail.com>
2014-04-25 16:43:04 +0200

    added code conventions

--- kidzen/app/controllers/friendships_controller.rb
+++ kidzen/app/controllers/friendships_controller.rb
@@ -1,16 +1,16 @@
 class FriendshipsController < ApplicationController
 
- def index
+  def index
 
- end
+  end
  
- def view_my_friends
+  def view_my_friends
  
- end
+  end
 
- def view_pending_friendship_requests
+  def view_pending_friendship_requests
 
- end
+  end
 
  # This method to send a friendship request.
  # user_id - the id of the child currently signed in.
@@ -21,20 +21,20 @@ class FriendshipsController < ApplicationController
  # Time complexity: O(n).
  #Authors: Khaled I. Elhossiny.
 
- def send_friend_request
-   user_id = current_user.id
-   friendship1 = Friendship.new( :child_1_id => params[:friend_id].to_i, :child_2_id => user_id, :status => "pending")
-   friendship2 = Friendship.new( :child_1_id => user_id, :child_2_id => params[:friend_id].to_i, :status => "requested")
-   if friendship1.save && friendship2.save
-    flash[:notice] = "friend request sent"
+  def send_friend_request
+    user_id = current_user.id
+    friendship1 = Friendship.new( :child_1_id => params[:friend_id].to_i, :child_2_id => user_id, :status => "pending")
+    friendship2 = Friendship.new( :child_1_id => user_id, :child_2_id => params[:friend_id].to_i, :status => "requested")
+    if friendship1.save && friendship2.save
+      flash[:notice] = "friend request sent"
    else 
-    flash[:error] = "unable to send friend request"
+      flash[:error] = "unable to send friend request"
    end
-   redirect_to( :controller => params[:cont].to_s, :action => params[:act].to_s)
- end
+    redirect_to( :controller => params[:cont].to_s, :action => params[:act].to_s)
+  end
 
- def accept_reject_friend_request
+  def accept_reject_friend_request
 
- end
+  end
 
 end
