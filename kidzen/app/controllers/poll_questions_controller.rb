bf1bfa03d0b635992797a660b2c23280b13b98d8
mrbasio <mr.basioo@gmail.com>
2014-04-25 06:09:38 +0200

    Issue #171 docc

--- kidzen/app/controllers/poll_questions_controller.rb
+++ kidzen/app/controllers/poll_questions_controller.rb
@@ -5,7 +5,7 @@ class PollQuestionsController < ApplicationController
   # GET /poll_questions.json
   #list all questions of the current loged in user
   #Parameters : non
-  #complixty : O(1)
+  #complixty : O(n)
   #return non
   #Author : Ahmad Bassiouny
   def index
@@ -32,7 +32,7 @@ class PollQuestionsController < ApplicationController
   # GET /poll_questions/new
   # create new poll Question and 1 answer after checking if the user is a 
   # logged in child or not
-  ##complixty : O(1)
+  ##complixty : O(n)
   #Parameters : non
   #return non
   #Author : Ahmad Bassiouny
@@ -63,7 +63,7 @@ class PollQuestionsController < ApplicationController
   # POST /poll_questions
   # POST /poll_questions.json
   #create poll with the questions and answers given by the user in the _form page
-  # #complixty : O(1)
+  # #complixty : O(n)
   #Parameters : non
   #return : redirect to the show page or the new page
   #Author : Ahmad Bassiouny
@@ -86,7 +86,7 @@ class PollQuestionsController < ApplicationController
   # PATCH/PUT /poll_questions/1
   # PATCH/PUT /poll_questions/1.json
   # update an exciting poll 
-  # complixty : O(1)
+  # complixty : O(n)
   #Parameters : non
   #return : redirect to the show or edit page
   #Author : Ahmad Bassiouny
@@ -118,11 +118,20 @@ class PollQuestionsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+  #Parameters : :id
+  #return : Poll Question object
+  #Author : Ahmad Bassiouny
+  #complixty : O(n)
+  # Use callbacks to share common setup or constraints between actions.
     def set_poll_question
       @poll_question = PollQuestion.find(params[:id])
     end
 
+    
+    #Parameters : __
+    #return : __
+    #Author : Ahmad Bassiouny
+    #complixty : O(1)
     # Never trust parameters from the scary internet, only allow the white list through.
     def poll_question_params
       params.require(:poll_question).permit(:content, :user_id)
@@ -130,6 +139,11 @@ class PollQuestionsController < ApplicationController
 
 
     #strong parameters required for rails 4 
+    #Parameters : __
+    #return : __
+    #Author : Ahmad Bassiouny
+    #complixty : O(1)
+    # Never trust parameters from the scary internet, only allow the white list through.
     def poll_question_params
       params.require(:poll_question).permit(:content, :user_id, poll_answers_attributes: [:content, :_destroy]) 
     end
