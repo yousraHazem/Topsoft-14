e73f544aa83a784a3f32404ab53750c64d0f9ec7
nouranMamdouhAbdo <nouran.mamdouh.33@gmail.com>
2014-04-25 14:02:47 +0200

    date

--- kidzen/app/controllers/events_controller.rb
+++ kidzen/app/controllers/events_controller.rb
@@ -102,6 +102,6 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:location, :name, :date_time, :description)
+      params.require(:event).permit(:location, :name, :date, :description)
     end
 end
