de70a3b22ea7c5d5081559ad9d959d0e8a2bdc04
--global <hussien.eloy@gmail.com>
2014-05-08 17:57:30 +0200

    Issue #285 add gallery with blueimp

--- kidzen/app/assets/javascripts/application.js
+++ kidzen/app/assets/javascripts/application.js
@@ -15,3 +15,8 @@
 //= require turbolinks
 //= require_tree .
 //= require cocoon
+//= require blueimp-gallery
+//= require blueimp-gallery-fullscreen
+//= require blueimp-gallery-indicator
+//= require blueimp-gallery-video
+//= require jquery.blueimp-gallery
