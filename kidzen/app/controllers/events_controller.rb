ebeb29ed24bb7bd4b9d00bebf9846223d3b97f79
nouranMamdouhAbdo <nouran.mamdouh.33@gmail.com>
2014-05-11 21:08:01 +0200

    Issue #319 add browse button in event view form and add variable image of type string in event_params in events controller

--- kidzen/app/controllers/events_controller.rb
+++ kidzen/app/controllers/events_controller.rb
@@ -100,7 +100,9 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:location, :name, :date_time, :description, :photoDescription, :image, :remote_image_url)
+      params.require(:event).permit(:location, :name, :date_time, :description, :image)
     end
+
+    
 
 end
