677ce3653e68e5ca6c0a8dfd9bcec236e7b7234c
madostar <medu2013@gmail.com>
2014-05-12 01:00:44 +0200

    Fixed destroy group bug, fixed show view, added error reporting for groups form and added documentation for changes. #279

--- kidzen/app/models/group.rb
+++ kidzen/app/models/group.rb
@@ -1,6 +1,10 @@
 class Group < ActiveRecord::Base
-  # Group Model , has a many to many relation with GroupMember
-  validates_presence_of :owner, :group_name, :group_description
-  has_and_belongs_to_many :group_members
-	searchkick autocomplete: [:group_name]
+  # Group Model , has one to many relation with GroupMember
+  # Authors: Nouran T. Attia and Mohammed T. Nabih
+  has_many :group_members
+  
+  #Validations
+  validates_presence_of :owner
+  validates :group_name, presence: true, length: { minimum: 4, maximum: 16}, uniqueness: true
+  validates :group_description, presence: true
 end
