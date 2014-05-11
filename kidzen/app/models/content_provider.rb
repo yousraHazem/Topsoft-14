43eab34c4463101b2aa1e3b7a3b512c3fd431e51
Nour2610 <nour.tarek2610@gmail.com>
2014-05-11 22:19:37 +0200

    including refactored code for sign in

--- kidzen/app/models/content_provider.rb
+++ kidzen/app/models/content_provider.rb
@@ -1,2 +1,12 @@
 class ContentProvider < ActiveRecord::Base
+  include TokenHelper
+
+  # Creates the token.
+  # Authors: Ahmed H. Ismail , Nouran Tarek Attia
+    def create_remember_token
+      self.remember_token = digest(new_remember_token)
+    end
+
+    before_create :create_remember_token
+
 end
