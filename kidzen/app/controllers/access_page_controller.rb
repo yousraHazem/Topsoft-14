b9f9436e93a054a3b38c5ca18cb9566328f36510
mohamedkhaledabdel <mohamed.kh.abdelmeguid@gmail.com>
2014-05-12 16:53:57 +0200

    Modified the code logic in html and controller, #275

--- kidzen/app/controllers/access_page_controller.rb
+++ kidzen/app/controllers/access_page_controller.rb
@@ -14,7 +14,8 @@ class AccessPageController < ApplicationController
   #	redirect_to session_path :new
   #end
  	end
- 	
+ 	#This action deletes the prevented tags from the tags list
+  #Author:- Mohamed Khaled Abdelmeguid
   #This action updates a child's options available upon the decision of the parent
   #Author:- Mohamed Khaled Abdelmeguid
   def update
@@ -22,12 +23,27 @@ class AccessPageController < ApplicationController
   @child = params[:child]
   @ability = params[:abilityUpdate]
   #new_hash = {'mutual_friends_rooms_only' => @value}
-  upd =Permission.find_by(registered_user_id: 2, abilities: ['mutual_friends_rooms_only' => !@value])
-  upd.update(:abilities => {'mutual_friends_rooms_only' => @value})
-  upd.save 
+  if @value == true
+    @upd = Permission.find_by(registered_user_id: 2, abilities: ['mutual_friends_rooms_only' => false])
+    if !@upd.nil?
+      @upd.update(:abilities => {'mutual_friends_rooms_only' => true})
+      @upd.save 
+    else
+      puts("error, No record with such option")
+    end  
+  end
+  if @value == false
+    @upd = Permission.find_by(registered_user_id: 2, abilities: ['mutual_friends_rooms_only' => true])
+    if !@upd.nil?
+      @upd.update(:abilities => {'mutual_friends_rooms_only' => false})
+      @upd.save 
+    else
+      puts("error, No record with such option")
+	  end
+  end  
   respond_to do |format|
   format.json { render json: {status: "ok"} }
 	end
-	end
-end
+  end
+end  
 
