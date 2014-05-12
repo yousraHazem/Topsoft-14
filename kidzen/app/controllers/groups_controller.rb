e8cd258c267eecc1fd2816335437777cadb988e7
madostar <medu2013@gmail.com>
2014-05-12 18:23:20 +0200

    Finished the first three tasks of the story #279

--- kidzen/app/controllers/groups_controller.rb
+++ kidzen/app/controllers/groups_controller.rb
@@ -25,8 +25,8 @@ class GroupsController < ApplicationController
   # Method for creating a new group.
   # Input : group_params hash.
   # Insertion of a new record in table groups.
-  # The function takes a unique username, and a group_name and ,
-  # creates a new group by creating and inserting a ,
+  # The function takes a unique username, and a group_name and,
+  # creates a new group by creating and inserting a,
   # new record in table groups.
   # Has error reporting for missing or misused fields.
   # Complexity: O(1).
--- kidzen/app/models/group.rb
+++ kidzen/app/models/group.rb
@@ -1,10 +1,10 @@
 class Group < ActiveRecord::Base
   # Group Model , has one to many relation with GroupMember
-  # Authors: Nouran T. Attia and Mohammed T. Nabih
+  # Authors: Nouran T. Attia, Mohammed T. Nabih
   has_many :group_members
   
-  #Validations
-  validates_presence_of :owner
+  # Validations
+  validates :owner, presence: true
   validates :group_name, presence: true, length: { minimum: 4, maximum: 16}, uniqueness: true
   validates :group_description, presence: true
 end
