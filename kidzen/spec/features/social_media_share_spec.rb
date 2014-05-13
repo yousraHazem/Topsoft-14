88422646ab620cd164b5a8f6e59e3b6df114e6ab
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2014-05-13 17:01:51 +0200

    Changes tests to follow capybara conventions #266.

--- kidzen/spec/features/social_media_share_spec.rb
+++ kidzen/spec/features/social_media_share_spec.rb
@@ -5,27 +5,14 @@ feature "Parent share on social media" do
 
   background do
     @user = create(:supervisor)
-
-  end
-
-  before do
     login(@user.registered_user.email, @user.registered_user.password)
     visit parent_profile_path unless current_path == parent_profile_path 
-    
   end
 
   subject { page }  
-
-  describe "Should be able to share via G+" do
-    it { should have_selector(".g-plus") }
-  end
-
-  describe "Should be able to tweet about the site" do
-    it { should have_selector(".twitter-share-button", text: 'Tweet') }
-  end
-
-  describe "Should be able to share on facebook" do
-    it { should have_selector(".fb-like") }
-  end
+    
+  scenario { should have_selector(".g-plus") }
+  scenario { should have_selector(".twitter-share-button", text: 'Tweet') }
+  scenario { should have_selector(".fb-like") } 
 
 end
