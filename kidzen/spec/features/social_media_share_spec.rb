03ff174c708d571c7a640329912da257784efcc2
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2014-05-13 15:43:54 +0200

    Adds tests for social plugins #266.

--- kidzen/spec/features/social_media_share_spec.rb
+++ kidzen/spec/features/social_media_share_spec.rb
@@ -1,20 +1,31 @@
 require 'spec_helper'
-
 # Minimal tests, just for checking if the buttons exist.
 # Authors: Ahmed H. Ismail.
 feature "Parent share on social media" do 
 
-
-  scenario "Should be able to share via G+" do
-
-  end
-
-  scenario "Should be able to tweet about the site" do
+  background do
+    @user = create(:supervisor)
 
   end
 
-  scenario "Should be able to share on facebook" do
+  before do
+    login(@user.registered_user.email, @user.registered_user.password)
+    visit parent_profile_path unless current_path == parent_profile_path 
+    
+  end
 
+  subject { page }  
+
+  describe "Should be able to share via G+" do
+    it { should have_selector(".g-plus") }
+  end
+
+  describe "Should be able to tweet about the site" do
+    it { should have_selector(".twitter-share-button", text: 'Tweet') }
+  end
+
+  describe "Should be able to share on facebook" do
+    it { should have_selector(".fb-like") }
   end
 
 end
