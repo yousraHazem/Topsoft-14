7350fa08ec4a45bf209445751565ee2a163d55bc
--global <hussien.eloy@gmail.com>
2014-05-14 00:12:09 +0200

    Issue #286 remove other  pushed by mistake files

--- kidzen/app/models/post.rb
+++
@@ -1,6 +0,0 @@
-class Post < ActiveRecord::Base
-# Here we ensure that every post's body will not be empty
-# also saying that every post has many comments
-  validates_presence_of :body
-  has_many :comments
-end
