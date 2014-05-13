5ece62d8555c1963d468d67bc7193da5934f0c27
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2014-05-13 08:05:11 +0200

    Sets up test enviroment #266.

--- kidzen/spec/factories.rb
+++ kidzen/spec/factories.rb
@@ -1,14 +1,15 @@
-FactoryGirl.define do
+  FactoryGirl.define do
   
-  Factory :parent_perm, class: Permission do
+  factory :parent_perm, class: Permission do
     abilities { { account_creation: true, supervisor: true, child: false } }
   end
 
-  Factory :child_perm, class: Permission do 
+
+  factory :child_perm, class: Permission do 
     abilities { { account_creation: false, supervisor: false, child: true } }
   end
 
-  Factory :parent_user, class: RegisteredUser do 
+  factory :parent_user, class: RegisteredUser do 
     username "supervisor1"
     first_name "mohamed"
     middle_name "ibrahim"
@@ -16,12 +17,12 @@ FactoryGirl.define do
     birth_date { 23.years.ago }
     gender "male"
     email "supervisor1@example.com"
-    association :permission, factory: :parent_perm
+    association :permission,  factory: :parent_perm
     password "foobar"
     password_confirmation "foobar"
   end
 
-  Factory :child_user, class: RegisteredUser do
+  factory :child_user, class: RegisteredUser do
     username "child1"
     first_name "engy"
     middle_name "mohamed"
@@ -29,15 +30,13 @@ FactoryGirl.define do
     birth_date { 16.years.ago 0 }
     gender "female"
     email "child1@example.com"
-    association :permission, factory: :child_perm
+    association :permission,  factory: :child_perm
     password "foobar"
     password_confirmation "foobar"
   end
 
-  Factory :supervisor, class  do 
+  factory :supervisor, class: Supervisor  do 
     association :registered_user, factory: :parent_user
   end
-
-
 
 end
