ff2d3502bb67c4671d394c96089dc6a69176eb23
--global <hussien.eloy@gmail.com>
2014-05-13 23:13:25 +0200

    Issue #286 Add documentation and fix code

--- kidzen/app/assets/javascripts/drag_and_drop.js
+++ kidzen/app/assets/javascripts/drag_and_drop.js
@@ -1,69 +1,123 @@
-  var matched = 0;
-  var nscore = 0;
-  var done = false;
+/*
+ * This is JavaScript file that handles all the actions that happen
+ * in The drag and drop game.
+ * Author : Hussien M. Eloy
+*/ 
+var matched = 0;
+var gameScore = 0;
+var canShare = false;
+
 $( init );
- 
+
+/*
+ * This function is used to start the game and reset all the variables
+ * and set all the object needs to be set according to its role in the game.
+ * Parameters : None.
+ * Returns : None.
+ * Approach : Just assigning the variables to zeros, 
+ * And assign the objects present in in game view that is needed to be
+ * used in the game in the game to their roles.
+ * Author : Hussien M. Eloy
+*/ 
 function init() {
-   matched = 0;
-   score = 0;
-   done = false;
-  $('.triangle').data('shape',1).draggable({
-    containment: '.contentn',
-    cursor: 'move',
-    snap: '.contentn'
-  });
-  $('#square').data('shape',2).draggable({
-    containment: '.contentn',
-    cursor: 'move',
-    snap: '.contentn'
-  });
-  $('#circle').data('shape',3).draggable({
-    containment: '.contentn',
-    cursor: 'move',
-   snap: '.contentn'
-  });
- $('#sqrdrop').data('shape',2).droppable({
-    drop: handhide
-  });
- $('#trdrop').data('shape',1).droppable({
-    drop: handhide
-  });
-  $('#crdrop').data('shape',3).droppable({
-    drop: handhide
-  });  
+    matched = 0;
+    gameScore = 0;
+    canShare = false;
+    $('.triangle').data('shape',1).draggable({
+        containment: '.contentn',
+        cursor: 'move',
+        snap: '.contentn'
+    });
+    $('#square').data('shape',2).draggable({
+        containment: '.contentn',
+        cursor: 'move',
+        snap: '.contentn'
+    });
+    $('#circle').data('shape',3).draggable({
+        containment: '.contentn',
+        cursor: 'move',
+        snap: '.contentn'
+    });
+    $('#square-slot').data('shape',2).droppable({
+        drop: playGame
+    });
+    $('#triangular-slot').data('shape',1).droppable({
+        drop: playGame
+    });
+    $('#circular-slot').data('shape',3).droppable({
+        drop: playGame
+    });  
 }
+
+/****************************************************
+* Creates an AJAX request. *
+* Always uses a put request. *
+* url - where to send *
+* data - payload is converted to json *
+* callback - function to be called when ready *
+* with params responseJSON, XMLHttpRequest object. *
+* Authors: Ahmed H. Ismail, Hussien M. Eloy *
+****************************************************/
 function create_ajax_request (url, data, callback) { 
-var xhr = new XMLHttpRequest(); 
-xhr.open('PUT', url, true); 
-xhr.setRequestHeader('Content-Type', 'application/json'); 
-xhr.setRequestHeader('Accept', 'application/json'); 
-xhr.responseType = "json"; xhr.addEventListener('load', function() { 
-xhr.responseJSON = xhr.response; console.log(xhr.responseJSON); 
-callback(xhr.responseJSON, xhr); }); 
- xhr.send( JSON.stringify(data) ); 
-  return xhr; 
+    var xhr = new XMLHttpRequest(); 
+    xhr.open('PUT', url, true); 
+    xhr.setRequestHeader('Content-Type', 'application/json'); 
+    xhr.setRequestHeader('Accept', 'application/json'); 
+    xhr.responseType = "json"; xhr.addEventListener('load', function() { 
+    xhr.responseJSON = xhr.response; console.log(xhr.responseJSON); 
+    callback(xhr.responseJSON, xhr); }); 
+    xhr.send( JSON.stringify(data) ); 
+    return xhr; 
 }
- function handhide(event ,ui) {
-  matched++;
-  var draggable = ui.draggable;
-  var contid = $(this).data('shape')
-  var shid = draggable.data('shape')
-  if(contid == shid) nscore++;
-  draggable.hide();
-  if(matched == 3) {
-    alert('Your Score: '+nscore);
-    done = true;
-    var url = ['http://' + location.host, 'drag_and_drops',n].join('/');
-    var callback = function(responseJSON, xhr) {}
-    create_ajax_request(url,{score: nscore},callback);
+
+/*
+ * This method is basically the engine of the game as it keeps
+ * track of actions taken so far, as well as score accomplished during
+ * the game, At the end it should send ajax request to the controller
+ * to insert the score of the last game in its table.
+ * Parameters : * event - (dropping draggable object in droppable one).
+ *              * ui - draggable object.
+ * Returns : None
+ * Author : Hussien M. Eloy
+*/
+function playGame(event, ui) {
+    matched++;
+    var draggable = ui.draggable;
+    var slotId = $(this).data('shape')
+    var shapeId = draggable.data('shape')
+    if (slotId == shapeId) 
+        gameScore++;
+    draggable.hide();
+    if (matched == 3) {
+        alert('Your Score: ' + gameScore);
+        canShare = true;
+        var url = ['http://' + location.host
+            , 'drag_and_drops', gameId].join('/');
+        var callback = function(responseJSON, xhr) {}
+        create_ajax_request(url, {score: gameScore}, callback);
+    }
 }
-}
+/*
+ * This method is used to make the user able to share
+ * his/her progress in the current game by sending a post
+ * containing the last score achieved by the user.
+ * Parameters : None.
+ * Returns : None.
+ * Approach : If the user can share score (i.e That he/she
+ * has already finished some game, A PUT request will be sent to the controller
+ * holding an automated messages as post title and content, They change only
+ * as they hold game (id - name) and score, The message carried with this
+ * request will be sent to DragAndDropsController to be inserted in post table.
+ * Author : Hussien M. Eloy   
+*/
 function shareScore() {
-  if(done) {
-    var url = ['http://' + location.host, 'drag_and_drops','share_score'].join('/');
-    var gurl = ['http://' + location.host, 'drag_and_drops',n].join('/');
-    var head = 'My progress at '+ g +'game';
-    var post = 'I have scored '+nscore+' points at ' +g.link(gurl);
+    if (canShare) {
+    var url = ['http://' + location.host, 'drag_and_drops'
+        , gameID].join('/');
+    var gameUrl = ['http://' + location.host, 'drag_and_drops'
+        , gameId].join('/');
+    var head = 'My progress at '+ gameName +'game';
+    var post = 'I have scored '+gameScore+' points at '+gameName.link(gameUrl);
     var callback = function(responseJSON, xhr) {}
     create_ajax_request(url,{title: head, body: post},callback);
     alert('Successfully Shared :)');
--- kidzen/app/controllers/drag_and_drops_controller.rb
+++ kidzen/app/controllers/drag_and_drops_controller.rb
@@ -1,42 +1,81 @@
 class DragAndDropsController < ApplicationController
-skip_before_filter :verify_authenticity_token, :only => [:update]
- protect_from_forgery with: :null_session
- require 'json'
+  skip_before_filter :verify_authenticity_token, :only => [:update]
+  protect_from_forgery with: :null_session
+  require 'json'
+
+  # This method is used to Allow accessing model attributes As Rails 4 does
+  # not support "attr_accessible" keyword.
+  # Parameters : None.
+  # Returns : None.
+  # Approach : The method is  just making The model's column accessible.
+  # Author : Hussien M. Eloy.
   def drag_and_drop_params
-	params.require(:drag_and_drop).permit(:name, :image)
+    params.require(:drag_and_drop).permit(:name, :image)
   end
 
+  # This method used to initialize new Drag and Drop Game 
+  # and assign it to variable @drag_and_drop in order to access it in new view.
+  # Parameters : None.
+  # Returns : None.
+  # Approach : Just creates new Instance of Game.
+  # Author : Hussien M. Eloy.
   def new
-  	@drag_and_drop = DragAndDrop.new
+    @drag_and_drop = DragAndDrop.new
   end
 
+  # This method used to assign game using it's id
+  # to variable @drag_and_drop in order to view it.
+  # as well as selecting the scores that corresponds ti this game and view the
+  # latest 10 of them in the show view.
+  # Parameters : None.
+  # Returns : None.
+  # Approach : Select game record using it's id.
+  # Author : Hussien M. Eloy.
   def show
-  	@drag_and_drop = DragAndDrop.find(params[:id])
-  	@drag_and_drops_scores = DragAndDropScore.order("created_at desc").limit(10).where(:drag_and_drop_id => @drag_and_drop[:id])
+    @drag_and_drop = DragAndDrop.find(params[:id])
+    @drag_and_drops_scores = DragAndDropScore
+      .order("created_at desc").limit(10)
+        .where(:drag_and_drop_id => @drag_and_drop[:id])
   end
 
+  # This method used to initialize new game using its parameters
+  # and assign it to variable @drag_and_drop and trying to save it
+  # in order to access it in form view to create.
+  # Parameters : None.
+  # Returns : None.
+  # Approach : Just creates new Instance of game.
+  # Author : Hussien M. Eloy.
   def create
-		@drag_and_drop = DragAndDrop.new(drag_and_drop_params)
-		if @drag_and_drop.save
-			flash[:notice] = "Successfully created drag & drop game."
-			redirect_to @drag_and_drop
-		else
-			render :action => 'new'
-		end
-	end
-	def update
-    @post_title = params[:title]
-    @post_content = params[:body]
-    @post = Post.new(:title => @post_title, :body => @post_content)
-    @post.save
-    @new_score =  params[:score]
-    @drag_and_drops_score = DragAndDropScore.new(:drag_and_drop_id => params[:id], :score => @new_score)
-    @drag_and_drops_score.save
+    @drag_and_drop = DragAndDrop.new(drag_and_drop_params)
+    if @drag_and_drop.save
+      flash[:notice] = "Successfully created drag & drop game."
+      redirect_to @drag_and_drop
+    else
+      render :action => 'new'
+    end
   end
-  def share_score
-    @post_title = params[:title]
-    @post_content = params[:body]
-    @post = Post.new(:title => @post_title, :body => @post_content)
+
+  # This method is used to recieve AJAX requests that come from the javascript
+  # files. In order to use these data for insertion in database in table Posts 
+  # or table DragAndDropScores.
+  # Parameters : None.
+  # Returns : None.
+  # Approach : This method waits for the AJAX request that 
+  # came from client side, if the user wants to share score, It will wait for
+  # PUT request that hold title and content of the sharing post 
+  # and save it later, If it happens at the end of the game it will recieve new
+  # PUT request that holds the score of the played game and the id of this game
+  # , then it should store them in DragAndDropScores table 
+  # in order to keep track of score of this turn when playing again. 
+  # Author : Hussien M. Eloy.
+  def update
+    post_title = params[:title]
+    post_content = params[:body]
+    @post = Post.new(:title => post_title, :body => post_content)
     @post.save
+    new_score =  params[:score]
+    @drag_and_drops_score = DragAndDropScore.new(:drag_and_drop_id => params[:id], 
+      :score => new_score)
+    @drag_and_drops_score.save
   end
 end
