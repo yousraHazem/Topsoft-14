b7ef69fefb051f6693727f0574794455b0eeac96
--global <hussien.eloy@gmail.com>
2014-05-13 19:43:51 +0200

    Issue #285 add documentation and fix code

--- kidzen/app/assets/javascripts/photo.js
+++ kidzen/app/assets/javascripts/photo.js
@@ -1,6 +1,25 @@
-function doLoc () {
+/*
+This function used in photo validation in case of uploading from 
+local machine.
+Parameters : None.
+Returns : None.
+Approach : If the image is uploaded from personal computer,
+The value of the field contains the remote image will be cleared.
+Author : Hussein M. Eloy
+*/
+function doLoc() {
     document.getElementById("rem").value = '';
-  }
-  function doRem() {
+}
+
+/*
+This function used in photo validation in case of uploading from 
+remote url.
+Parameters : None.
+Returns : None.
+Approach : If the image is uploaded from remote url, 
+The value of the field contains the local image will be cleared.
+Author : Hussein M. Eloy
+*/
+function doRem() {
     document.getElementById("loc").value = '';
-  }
+}
