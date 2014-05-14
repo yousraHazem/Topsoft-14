9434f6d7ed6776d29789065b55d21038876d0e75
nouranMamdouhAbdo <nouran.mamdouh.33@gmail.com>
2014-05-14 14:58:50 +0200

    Issue #312 Documentations for event controller and Associations and validations in child model

--- kidzen/app/models/child.rb
+++ kidzen/app/models/child.rb
@@ -1,6 +1,29 @@
 class Child < ActiveRecord::Base
   # Associations and validations.
   belongs_to :registered_user, dependent: :destroy
+  has_many :invitation_events
+           :foreign_key => 'inviter_id',
+           :dependent => :destroy
+
+  has_many :invited_friends,
+           :through => :friendships,
+           :conditions => "accept = 'accepted'"
+
+  has_many :requested_friends,
+           :through => :friendships,
+           :source => :friend,
+           :conditions => "status = 'requested'",
+           :order => :created_at
+
+  has_many :pending_friends,
+           :through => :friendships,
+           :source => :friend,
+           :conditions => "status = 'pending'",
+           :order => :created_at
+
+  has_many :friendships,
+           :foreign_key => 'child_1_id',
+           :dependent => :destroy
 
 
   # Checks if other is a friend of this child.
