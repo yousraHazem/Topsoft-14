61f6d111361754a56cd4a2d3db49b2004f4bd183
Nour2610 <nour.tarek2610@gmail.com>
2014-05-15 18:05:57 +0200

    Adding show action and filtering #306

--- kidzen/app/controllers/content_providers_controller.rb
+++ kidzen/app/controllers/content_providers_controller.rb
@@ -1,10 +1,19 @@
 class ContentProvidersController < ApplicationController
-  def new
+  include SessionsHelper
+
+  before_filter :set_content_provider
+
+  def set_content_provider
+    if signed_in? && ContentProvider.exists?(username:
+      current_user.username)
+      @content_provider = current_user
+    else
+      redirect_to session_path :new
+    end
   end
 
-  def create
-  end
-
+  # Renders the content provider's homepage
+  # Authors: Nouran T. Attia
   def show
   end
 end
