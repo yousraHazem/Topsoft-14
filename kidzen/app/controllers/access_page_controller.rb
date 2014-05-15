530e849abf14f450c84496fc10802ff3304304f4
mohamedkhaledabdel <mohamed.kh.abdelmeguid@gmail.com>
2014-05-15 20:50:47 +0200

    Update access_page_controller.rb
    @sharybeshara done with the convetions

--- kidzen/app/controllers/access_page_controller.rb
+++ kidzen/app/controllers/access_page_controller.rb
@@ -1,12 +1,12 @@
 #Access Control Page
-#Author:- Mohamed Khaled Abdelmeguid
+#Authors:- Mohamed Khaled Abdelmeguid
 
 class AccessPageController < ApplicationController
-	skip_before_filter :verify_authenticity_token, only: [:delete_tag]
+  skip_before_filter :verify_authenticity_token, only: [:delete_tag]
   skip_before_filter :verify_authenticity_token, only: [:update]
-    skip_before_filter :verify_authenticity_token, only: [:add]
-    skip_before_filter :verify_authenticity_token, only: [:update_join_rooms]
-    skip_before_filter :verify_authenticity_token, only: [:update_create_rooms]
+  skip_before_filter :verify_authenticity_token, only: [:add]
+  skip_before_filter :verify_authenticity_token, only: [:update_join_rooms]
+  skip_before_filter :verify_authenticity_token, only: [:update_create_rooms]
 
   #This action is used to show the prevented Tags
   #Author:- Mohamed Khaled Abdelmeguid
@@ -15,16 +15,14 @@ class AccessPageController < ApplicationController
   @banned = Keyword.new(params[:tag])
   @child = Child.first
   #else
-  #	redirect_to session_path :new
+  #redirect_to session_path :new
   #end
- 	end
- 	#This action deletes the prevented tags from the tags list
+  end
+  #This action deletes the prevented tags from the tags list
   #Author:- Mohamed Khaled Abdelmeguid
-  
   def update_create_rooms
   @value = params[:value]
   @child = params[:child]
-  #new_hash = {'mutual_friends_rooms_only' => @value}
   if @value == true
     @upd = Permission.find_by(registered_user_id: 2, abilities: ['can_create_public_chat_rooms' => false])
     if !@upd.nil?
@@ -47,10 +45,5 @@ class AccessPageController < ApplicationController
   format.json { render json: {status: "ok"} }
   end
   end
-  #This action is used to add a new prevented tag by submitting it
-  #and we first check if it's already found in the DB
-  #Author:- Mohamed Khaled AbdelMeguid
-  
-
-end  
+ end  
 
