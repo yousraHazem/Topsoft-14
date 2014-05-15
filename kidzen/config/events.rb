f9f3e438d71fb895e66753ab3db32719f73c463e
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2014-05-15 11:56:25 +0200

    Formats events.rb file #264.

--- kidzen/config/events.rb
+++ kidzen/config/events.rb
@@ -1,15 +1,12 @@
 WebsocketRails::EventMap.describe do
   # Notification Events start.
   # Authors: Ahmed H. Ismail.
-
   namespace :notifications do
-
     subscribe :pending, 'notifications#pending'
     subscribe :index, 'notifications#index'
     subscribe :mark_read, 'notifications#mark_read'
     subscribe :mark_unread, 'notifications#mark_unread'
-    subscribe :subscribe, 'notifications#subscribe'
-    
+    subscribe :subscribe, 'notifications#subscribe'    
   end
   # Notification Events end.
 
