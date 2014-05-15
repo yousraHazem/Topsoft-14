15b9f503effe5f2e655e851146cbf84ae6bbdbaf
khaled-barca <khaled.elhossiny10@gmail.com>
2014-05-15 23:55:11 +0200

    added method available in activity model #307

--- kidzen/app/models/activity.rb
+++ kidzen/app/models/activity.rb
@@ -3,4 +3,8 @@ class Activity < ActiveRecord::Base
 belongs_to :topic
 has_many :activity_accounts
 has_many :children, :through=>:activity_accounts
+
+  def self.available(age)
+    where("activities.min_age<=? AND activities.max_age>=?",age,age)
+  end
 end
