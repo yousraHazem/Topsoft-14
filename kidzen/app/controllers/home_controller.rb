6d1cb5bad41992286a8c77527e442fc5bce59033
alloka <alihalawaty@gmail.com>
2014-05-16 20:09:36 +0200

    documentation

--- kidzen/app/controllers/home_controller.rb
+++ kidzen/app/controllers/home_controller.rb
@@ -1,31 +1,27 @@
 require 'securerandom'
 class HomeController < ApplicationController
 
-    def recording
 
-    end
-
+    # This function creates a new recording.
+    # @files is initial variable of type file
+    # Author: Ali A. Halawaty.
     def record
         @files = AudioFiles.where(ip: request.remote_ip.to_s).order("created_at DESC")
     end
-
+    # This function saves the recording in the computer of the user.
+    # Author: Ali A. Halawaty.
     def saveRecording
-
-        audio = params["record"]["audio"]
-
-        filename = SecureRandom.hex
-
-        save_path = Rails.root.join("public/audioClips/#{filename}.wav")
-
-        File.open(save_path,"wb") do |f|
-            f.write audio.read
-        end
-
-        AudioFiles.create(ip: request.remote_ip.to_s, filename: filename)
-
-        render nothing: true
+      audio = params["record"]["audio"]
+      filename = SecureRandom.hex
+      save_path = Rails.root.join("public/audioClips/#{filename}.wav")
+      File.open(save_path,"wb") do |f|
+        f.write audio.read
+      end
+      AudioFiles.create(ip: request.remote_ip.to_s, filename: filename)
+      render nothing: true
     end
-
+    # This function deletes the recording.
+    # Author: Ali A. Halawaty.
     def delete
         file = AudioFiles.find(params[:id])
         if(file.ip == request.remote_ip)
@@ -36,7 +32,5 @@ class HomeController < ApplicationController
         redirect_to(action: "record")
     end
 
-    def recOGG
-    end
 
 end
