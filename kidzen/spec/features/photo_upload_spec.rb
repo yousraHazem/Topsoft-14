1b9b36cde6663f1edd2dd4d147697baaf31a6911
--global <hussien.eloy@gmail.com>
2014-05-16 22:02:37 +0200

    Issue #285 add capybara

---
+++ kidzen/spec/features/photo_upload_spec.rb
@@ -0,0 +1,15 @@
+require "spec_helper"
+
+feature "Photo uploading" do
+  scenario "User uploads new photo" do
+    visit "/photos/new"
+
+    fill_in "Description", :with => "Test Description"
+    fill_in "remote_image_url", 
+      :with => "http://images4.fanpop.com/image/photos/18500000"\
+        "/Beautiful-Color-roses-18577527-500-375.jpg"
+    click_button "Create Photo"
+
+    expect(page).to have_text("Successfully created photo.")
+  end
+end
