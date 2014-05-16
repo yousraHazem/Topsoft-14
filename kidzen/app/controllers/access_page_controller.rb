b8582b4be1cf2526601ad1e8f7d75c2722f3d4f7
mohamedkhaledabdel <mohamed.kh.abdelmeguid@gmail.com>
2014-05-16 21:50:44 +0200

    Update access_page_controller.rb
    Done with conventions #275

--- kidzen/app/controllers/access_page_controller.rb
+++ kidzen/app/controllers/access_page_controller.rb
@@ -1,13 +1,14 @@
-#Access Control Page
-#Authors:- Mohamed Khaled Abdelmeguid
+# Access Control Page.
+# Authors: Mohamed Khaled Abdelmeguid.
 
 class AccessPageController < ApplicationController
   skip_before_filter :verify_authenticity_token, only: [:delete_tag]
   skip_before_filter :verify_authenticity_token, only: [:update_mutual_rooms]
   skip_before_filter :verify_authenticity_token, only: [:add]
-  #This action is used to show the prevented Tags
-  #Authors:- Mohamed Khaled Abdelmeguid
-  #Child.first will be replaced by params [dependency waiting]
+  
+  #This action is used to show the prevented Tags.
+  #Authors:- Mohamed Khaled Abdelmeguid.
+  #Child.first will be replaced by params [dependency waiting].
   def access
   if signed_in?
     @child = Child.find_by(registered_user_id: params[:id])
@@ -15,9 +16,9 @@ class AccessPageController < ApplicationController
     redirect_to session_path :new
   end
   end
-  #This action changes an option value of a child in the DB
-  #according to his parent choice
-  #Authors:- Mohamed Khaled AbdelMeguid
+  # This action changes an option value of a child in the DB
+  # according to his parent choice.
+  # Authors: Mohamed Khaled AbdelMeguid.
   def update_mutual_rooms
   @value = params[:value]
   @child = params[:child]
@@ -42,7 +43,7 @@ class AccessPageController < ApplicationController
     end
   end  
   respond_to do |format|
-  format.json { render json: {status: "ok"} }
+    format.json { render json: {status: "ok"} }
   end
   end
 end  
