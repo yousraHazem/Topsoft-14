b02534b764d1e3b29bd253403abcafe68bb3dfed
Nour2610 <nour.tarek2610@gmail.com>
2014-05-16 21:06:46 +0200

    Revert "Adding show action and filtering #306"
    This reverts commit 61f6d111361754a56cd4a2d3db49b2004f4bd183.

--- kidzen/app/controllers/content_providers_controller.rb
+++ kidzen/app/controllers/content_providers_controller.rb
@@ -1,19 +1,10 @@
 class ContentProvidersController < ApplicationController
-  include SessionsHelper
-
-  before_filter :set_content_provider
-
-  def set_content_provider
-    if signed_in? && ContentProvider.exists?(username:
-      current_user.username)
-      @content_provider = current_user
-    else
-      redirect_to session_path :new
-    end
+  def new
   end
 
-  # Renders the content provider's homepage
-  # Authors: Nouran T. Attia
+  def create
+  end
+
   def show
   end
 end
