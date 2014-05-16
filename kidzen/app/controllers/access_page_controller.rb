7e4be52e228e91e7f58a66c4419fc5fad6989c00
mohamedkhaledabdel <mohamed.kh.abdelmeguid@gmail.com>
2014-05-16 20:35:42 +0200

    Update access_page_controller.rb
    removed unused line

--- kidzen/app/controllers/access_page_controller.rb
+++ kidzen/app/controllers/access_page_controller.rb
@@ -2,20 +2,19 @@
 #Authors:- Mohamed Khaled Abdelmeguid
 
 class AccessPageController < ApplicationController
-	skip_before_filter :verify_authenticity_token, only: [:delete_tag]
+  skip_before_filter :verify_authenticity_token, only: [:delete_tag]
   skip_before_filter :verify_authenticity_token, only: [:update_mutual_rooms]
   skip_before_filter :verify_authenticity_token, only: [:add]
-
   #This action is used to show the prevented Tags
   #Authors:- Mohamed Khaled Abdelmeguid
   #Child.first will be replaced by params [dependency waiting]
   def access
   if signed_in?
-  @child = Child.find_by(registered_user_id: params[:id])
+    @child = Child.find_by(registered_user_id: params[:id])
   else
     redirect_to session_path :new
   end
- 	end
+  end
   #This action changes an option value of a child in the DB
   #according to his parent choice
   #Authors:- Mohamed Khaled AbdelMeguid
