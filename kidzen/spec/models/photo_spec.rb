fb61d67f16c5e3ce919eb05be3d4a93bcc2c7fbe
--global <hussien.eloy@gmail.com>
2014-05-16 21:35:02 +0200

    Issue #285 add more tests

--- kidzen/spec/models/photo_spec.rb
+++ kidzen/spec/models/photo_spec.rb
@@ -3,7 +3,19 @@ require 'spec_helper'
 describe Photo do
   it 'is valid with a description and image' do
     photo = Photo.new(
-    description: 'This is a test assignemnt',remote_image_url: 'http://bigwol.com/wp-content/uploads/2014/05/Roses.jpg')
-    expect(photo).to be_valid
+      description: 'test photo',
+        remote_image_url: 'http://bigwol.com/wp-content/uploads/2014/05/Roses.jpg')
+          expect(photo).to be_valid
   end 
+  it 'is not valid without description' do
+     photo = Photo.new(
+       remote_image_url: 'http://bigwol.com/wp-content/uploads/2014/05/Roses.jpg')
+         expect(photo).not_to be_valid
+  end
+  it 'is not valid without image' do
+    photo = Photo.new(
+      description: 'Test photo')
+        expect(photo).not_to be_valid
+  end 
+  
 end
