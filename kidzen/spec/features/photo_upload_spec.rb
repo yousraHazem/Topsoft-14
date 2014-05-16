06383ca2bb5688e2d9f7edbf7375bb3e575724e7
--global <hussien.eloy@gmail.com>
2014-05-16 23:22:13 +0200

    Issue #285 add more tests

--- kidzen/spec/features/photo_upload_spec.rb
+++ kidzen/spec/features/photo_upload_spec.rb
@@ -2,9 +2,9 @@ require "spec_helper"
 
 feature "Photo uploading" do
   scenario "User uploads new photo" do
-    visit "/photos/new" puts page.body
-    fill_in "Description", :with => "Test Description"
-    fill_in "rem", 
+    visit "/photos/new" 
+    fill_in "description", :with => "Test Description"
+    fill_in "remote_image_url", 
       :with => "http://images4.fanpop.com/image/photos/18500000"\
         "/Beautiful-Color-roses-18577527-500-375.jpg"
     click_button "Create Photo"
