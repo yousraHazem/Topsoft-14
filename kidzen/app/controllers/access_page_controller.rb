a24a3813ae62505b9851e965d6b36225e4473399
mohamedkhaledabdel <mohamed.kh.abdelmeguid@gmail.com>
2014-05-16 22:10:24 +0200

    Update access_page_controller.rb
    Done with conventions #274

--- kidzen/app/controllers/access_page_controller.rb
+++ kidzen/app/controllers/access_page_controller.rb
@@ -1,5 +1,5 @@
-#Access Control Page
-#Author:- Mohamed Khaled Abdelmeguid
+# Access Control Page.
+# Author: Mohamed Khaled Abdelmeguid.
 
 class AccessPageController < ApplicationController
   skip_before_filter :verify_authenticity_token, only: [:delete_tag]
@@ -7,41 +7,45 @@ class AccessPageController < ApplicationController
   skip_before_filter :verify_authenticity_token, only: [:add]
   skip_before_filter :verify_authenticity_token, only: [:update_join_rooms]
   skip_before_filter :verify_authenticity_token, only: [:update_create_rooms]
-  #This action is used to show the prevented Tags
-  #Authors:- Mohamed Khaled Abdelmeguid
+  
+  # This action is used to show the prevented Tags.
+  # Authors: Mohamed Khaled Abdelmeguid.
   def access
-  if signed_in?
-    @child = Child.find_by(registered_user_id: params[:id])
-  else
-    redirect_to session_path :new
-  end
-  end
-  #This action updates the child option with true or false 
-  #Authors:- Mohamed Khaled Abdelmeguid
-  def update_create_rooms
-  @value = params[:value]
-  @child = params[:child]
-  if @value == true
-    @upd = Permission.find_by(registered_user_id: @child, abilities: ['can_create_public_chat_rooms' => false])
-    if !@upd.nil?
-      @upd.update(:abilities => {'can_create_public_chat_rooms' => true})
-      @upd.save 
+    if signed_in?
+      @child = Child.find_by(registered_user_id: params[:id])
     else
-      puts("error, No record with such option")
-    end  
-  end
-  if @value == false
-    @upd = Permission.find_by(registered_user_id: @child, abilities: ['can_create_public_chat_rooms' => true])
-    if !@upd.nil?
-      @upd.update(:abilities => {'can_create_public_chat_rooms' => false})
-      @upd.save 
-    else
-      puts("error, No record with such option")
+      redirect_to session_path :new
     end
-  end  
-  respond_to do |format|
-  format.json { render json: {status: "ok"} }
   end
-  end
+  
+  # This action updates the child option with true or false.
+  # Authors: Mohamed Khaled Abdelmeguid.
+  def update_create_rooms
+    @value = params[:value]
+    @child = params[:child]
+    if @value == true
+      @upd = Permission.find_by(registered_user_id: @child, 
+        abilities: ['can_create_public_chat_rooms' => false])
+      if !@upd.nil?
+        @upd.update(:abilities => {'can_create_public_chat_rooms' => true})
+        @upd.save 
+      else
+        puts("error, No record with such option")
+      end  
+    end
+    if @value == false
+      @upd = Permission.find_by(registered_user_id: @child, 
+        abilities: ['can_create_public_chat_rooms' => true])
+      if !@upd.nil?
+        @upd.update(:abilities => {'can_create_public_chat_rooms' => false})
+        @upd.save 
+      else
+        puts("error, No record with such option")
+      end
+    end  
+    respond_to do |format|
+      format.json { render json: {status: "ok"} }
+    end
+    end
 end  
 
