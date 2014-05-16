66077a0ccac647653a403868a8cc9d20204b8e25
sharybeshara <sharybeshara@gmail.com>
2014-05-16 23:38:36 +0300

    Update post.rb

--- kidzen/app/models/post.rb
+++ kidzen/app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
-# Here we ensure that every post's body will not be empty
-# also saying that every post has many comments
+  # Here we ensure that every post's body will not be empty
+  # also saying that every post has many comments
   validates_presence_of :body
   has_many :comments
 end
