73662843c0ec104af71acc8d8ba2f3efb12bbd7e
Nour2610 <nour.tarek2610@gmail.com>
2014-05-16 23:22:22 +0200

    Modifying the controller, model, and show view for content provider #306

--- kidzen/app/controllers/content_providers_controller.rb
+++ kidzen/app/controllers/content_providers_controller.rb
@@ -1,10 +1,15 @@
 class ContentProvidersController < ApplicationController
-  def new
-  end
-
-  def create
-  end
 
   def show
+    if signed_in?
+      # Is user a supervisor?
+      if ContentProvider.exists?(registered_user: current_user)
+        @user = ContentProvider.find(current_user)
+        # Render view
+      end
+    else
+      # No one signed in
+      redirect_to session_path :new
+    end
   end
 end
--- kidzen/app/models/content_provider.rb
+++ kidzen/app/models/content_provider.rb
@@ -1,2 +1,7 @@
+# ContentProvider model.
+# Authors: Ahmed H. Ismail, Nouran T. Attia.
 class ContentProvider < ActiveRecord::Base
+  has_one :registered_user
+  has_many :activities
+  self.primary_key = 'registered_user_id'
 end
