461bc6dff2a1b4b96c07d5ed78ce4f910046b4cc
mohamedkhaledabdel <mohamed.kh.abdelmeguid@gmail.com>
2014-05-16 21:56:51 +0200

    Update access_page_controller.rb
    Done with conventions #273

--- kidzen/app/controllers/access_page_controller.rb
+++ kidzen/app/controllers/access_page_controller.rb
@@ -11,41 +11,41 @@ class AccessPageController < ApplicationController
   # This action is used to show the prevented Tags.
   # Authors: Mohamed Khaled Abdelmeguid.
   def access
-  if signed_in?
-    @child = Child.find_by(registered_user_id: params[:id])
-  else
-    redirect_to session_path :new
-  end
+    if signed_in?
+      @child = Child.find_by(registered_user_id: params[:id])
+    else
+      redirect_to session_path :new
+    end
   end
   
   # This action updates a child's options available upon the decision of the parent.
   # Author:- Mohamed Khaled Abdelmeguid.
   def update_join_rooms
-  @value = params[:value]
-  @child = params[:child]
-  if @value == true
-    @upd = Permission.find_by(registered_user_id: @child, 
-      abilities: ['can_join_public_chat_rooms' => false])
-    if !@upd.nil?
-      @upd.update(:abilities => {'can_join_public_chat_rooms' => true})
-      @upd.save 
-    else
-      puts("error, No record with such option")
-    end  
-  end
-  if @value == false
-    @upd = Permission.find_by(registered_user_id: @child, 
-      abilities: ['can_join_public_chat_rooms' => true])
-    if !@upd.nil?
-      @upd.update(:abilities => {'can_join_public_chat_rooms' => false})
-      @upd.save 
-    else
-      puts("error, No record with such option")
+    @value = params[:value]
+    @child = params[:child]
+    if @value == true
+      @upd = Permission.find_by(registered_user_id: @child, 
+        abilities: ['can_join_public_chat_rooms' => false])
+      if !@upd.nil?
+        @upd.update(:abilities => {'can_join_public_chat_rooms' => true})
+        @upd.save 
+      else
+        puts("error, No record with such option")
+      end  
     end
-  end  
-  respond_to do |format|
-    format.json { render json: {status: "ok"} }
-  end
+    if @value == false
+      @upd = Permission.find_by(registered_user_id: @child, 
+        abilities: ['can_join_public_chat_rooms' => true])
+      if !@upd.nil?
+        @upd.update(:abilities => {'can_join_public_chat_rooms' => false})
+        @upd.save 
+      else
+        puts("error, No record with such option")
+      end
+    end  
+    respond_to do |format|
+      format.json { render json: {status: "ok"} }
+    end
   end
 end  
 
