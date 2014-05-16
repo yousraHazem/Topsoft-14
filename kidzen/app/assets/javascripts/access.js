6cfca4951d4b7ad6b66399570aaaf4177419b426
mohamedkhaledabdel <mohamed.kh.abdelmeguid@gmail.com>
2014-05-16 21:40:36 +0200

    Update access.js
    Done with conventions @sharybeshara @ah450

--- kidzen/app/assets/javascripts/access.js
+++ kidzen/app/assets/javascripts/access.js
@@ -1,7 +1,8 @@
-//This function creates an AJAX request where the paramaters are the url which is specifing
-//the controller and the action, the data which is specifing the data to be sent to the 
-//controller, and a callback function
-//Authors:- Mohamed Khaled AbdelMeguid
+/* This function creates an AJAX request where the paramaters are the url which is specifing
+the controller and the action, the data which is specifing the data to be sent to the 
+controller, and a callback function.
+Authors: Mohamed Khaled AbdelMeguid. */
+
 function create_ajax_request (url, data, callback) {
     var xhr = new XMLHttpRequest();
     xhr.open('PUT', url, true);
@@ -17,9 +18,9 @@ function create_ajax_request (url, data, callback) {
     return xhr;
 }
 
-//This function change a child option when the checkbox is pressed with a true or false value
-//in the DB through an AJAX request
-//Authors:- Mohamed Khaled AbdelMeguid
+/*This function change a child option when the checkbox is pressed with a true or false value
+in the DB through an AJAX request.
+Authors:- Mohamed Khaled AbdelMeguid. */
  function changeJoinRooms(childId) {
     var checkBox = document.getElementById("can join public chat rooms");
     var valueUpdate = checkBox.checked;
