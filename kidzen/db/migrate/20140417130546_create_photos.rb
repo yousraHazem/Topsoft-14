da1f30a7d6d73542bbcd0728f6f228d266cdbd8e
AbdelrahmanSaad <sonic_goku@hotmail.com>
2014-05-16 22:38:28 +0200

    Issue #414 removing migration files

--- kidzen/db/migrate/20140417130546_create_photos.rb
+++ kidzen/db/migrate/20140417130546_create_photos.rb
@@ -1,6 +1,7 @@
 class CreatePhotos < ActiveRecord::Migration
   def change
     create_table :photos do |t|
+        t.references :post, index: true
       	t.string :description
       	t.string :image
       	t.string :remote_image_url
