befb792ca6ab9e7989d067167817cc6c59fa165d
sharybeshara <sharybeshara@gmail.com>
2014-05-16 23:21:19 +0300

    Update comments_controller.rb

--- kidzen/app/controllers/comments_controller.rb
+++ kidzen/app/controllers/comments_controller.rb
@@ -1,10 +1,10 @@
 class CommentsController < ApplicationController
-#Here we create our comment
-#Parameters : None
-#returns : None
+# Here we create our comment
+# Parameters : None
+# returns : None
 # This method checks if the user is not a supervisor to be allowed to create a 
 # new comment.
-#Author : Abdelrahman Saad, Shary Beshara 
+# Authors : Abdelrahman Saad, Shary Beshara 
   def create
   	if ! Supervisor.exists?(registered_user: current_user)
     	@post = Post.find(params[:post_id])
@@ -12,10 +12,10 @@ class CommentsController < ApplicationController
     	redirect_to @post
     end
   end
-#Here we define the parameters of the comment
-#Parameters : None
-#returns : None
-#Author : Abdelrahman Saad
+# Here we define the parameters of the comment
+# Parameters : None
+# returns : None
+# Authors : Abdelrahman Saad
   def comment_params
     params.require(:comment).permit(:body , :post_id)
   end
