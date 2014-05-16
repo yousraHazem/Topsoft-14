9c7ce62b201db6049952812a8411755cf657e8ee
madostar <medu2013@gmail.com>
2014-05-16 23:45:50 +0200

    Updated the story tasks #280

--- kidzen/app/controllers/group_members_controller.rb
+++ kidzen/app/controllers/group_members_controller.rb
@@ -117,6 +117,48 @@ class GroupMembersController < ApplicationController
        :owner_accept_state => 1, :member_accept_state => 1)
   end
 
+  # This is a function that adds a member to the group by his name.
+  # user is a variable of type RegisteredUser.
+  # child is a variable of type Child.
+  # member is a variavle of type GroupMember.
+  # Time complexity: O(1).
+  # Authors: Mohammed T. Nabih.
+  def add_member_name
+    user = RegisteredUser.all.where(:first_name => params[:first_name] , :last_name => params[:last_name] , :username => params[:username])
+    child = Child.where(:registered_user_id => user.id)
+    member = GroupMember.new(:group_id => :group_id,:username => user.username,:member_accept_state => 2,:owner_accept_state => 1)
+    member.save
+    end
+    
+
+  # This is a function that adds a member to the group by his email.
+  # Doesn't return anything.
+  # user is a variable of type RegisteredUser.
+  # child is a variable of type Child.
+  # member is a variable of type GroupMember.
+  # Time complexity: O(n).
+  # Authors: Mohammed T. Nabih.
+  def add_member_email
+    user = RegisteredUser.all.where(:email => params[:email])
+    child = Child.where(:registered_user_id => user.id)
+    member = GroupMember.new(:group_id => :group_id,:username => user.username,:member_accept_state => 2,:owner_accept_state => 1)
+    member.save
+  end
+
+  # This is a function that adds a member to the group by his phone number.
+  # Doesn't return anything.
+  # user is a variable of type RegisteredUser.
+  # child is a variable of type Child.
+  # member is a variable of type GroupMember.
+  # Time complexity: O(n).
+  # Authors: Mohammed T. Nabih.
+  def add_member_phone
+    user = RegisteredUser.all.where(:phone => params[:phone])
+    child = Child.where(:registered_user_id => user.id)
+    member = GroupMember.new(:group_id => :group_id,:username => user.username,:member_accept_state => 2,:owner_accept_state => 1)
+    member.save
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_group_member
