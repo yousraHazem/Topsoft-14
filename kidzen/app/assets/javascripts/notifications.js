f1f0408138112cf27c128cf5dde5e49a1a70f578
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2014-05-16 20:07:34 +0200

    Fixes notification.js indentation #266.

--- kidzen/app/assets/javascripts/notifications.js
+++ kidzen/app/assets/javascripts/notifications.js
@@ -14,7 +14,7 @@ notifications_connection.bind("notifications.new", new_notification);
  * Authors: Ahmed H. Ismail.              *
  ******************************************/
 function pending_event (notifications) {
-  notifications.forEach(new_notification);
+    notifications.forEach(new_notification);
 }
 
 
@@ -23,7 +23,7 @@ function pending_event (notifications) {
  * Authors: Ahmed H. Ismail.                   *
  ***********************************************/
 function unsubscribe() {
-  notifications_connection.trigger("notifications.unsubscribe");
+    notifications_connection.trigger("notifications.unsubscribe");
 }
 
 
@@ -32,7 +32,7 @@ function unsubscribe() {
  * Authors: Ahmed H. Ismail.        *
  ************************************/
 function subscribe() {
-  notifications_connection.trigger("notifications.subscribe");
+    notifications_connection.trigger("notifications.subscribe");
 }
 
 /******************************
@@ -41,18 +41,18 @@ function subscribe() {
  ******************************/
 function new_notification(notification) {
 
-  var notificiations_list = document.getElementById('actual_notifications');
-  var li = document.createElement('li');
-  li.appendChild(notification_pretty_printer(notification));
-  notificiations_list.appendChild(li);
+    var notificiations_list = document.getElementById('actual_notifications');
+    var li = document.createElement('li');
+    li.appendChild(notification_pretty_printer(notification));
+    notificiations_list.appendChild(li);
 
-  if(notification.custom_view_url != null) {
-    li.onclick = function() {
-      window.open(notification.custom_view_url); 
+    if(notification.custom_view_url != null) {
+        li.onclick = function() {
+            window.open(notification.custom_view_url); 
+        }
+
+        li.classList.add('clickable_notification');
     }
-
-    li.classList.add('clickable_notification');
-  }
 }
 
 
@@ -61,28 +61,26 @@ function new_notification(notification) {
  *                                                         *
  ***********************************************************/
 function notification_pretty_printer(notification) {
-  var div = document.createElement('div');
-  var title = document.createTextNode(notification.title);
-  var title_p = document.createElement('p');
-  title_p.appendChild(title);
-  title_p.classList.add('title');
-  var short_desc = document.createTextNode(notification.short_desc);
-  var sd_p = document.createElement('p');
-  sd_p.appendChild(short_desc);
-  sd_p.classList.add('short_desc');
+    var div = document.createElement('div');
+    var title = document.createTextNode(notification.title);
+    var title_p = document.createElement('p');
+    title_p.appendChild(title);
+    title_p.classList.add('title');
+    var short_desc = document.createTextNode(notification.short_desc);
+    var sd_p = document.createElement('p');
+    sd_p.appendChild(short_desc);
+    sd_p.classList.add('short_desc');
 
-  var long_desc = document.createTextNode(notification.long_desc);
-  var ld_p = document.createElement('p');
-  ld_p.appendChild(long_desc);
-  ld_p.classList.add('long_desc');
-  div.appendChild(title_p);
-  div.appendChild(sd_p);
-  div.appendChild(ld_p);
+    var long_desc = document.createTextNode(notification.long_desc);
+    var ld_p = document.createElement('p');
+    ld_p.appendChild(long_desc);
+    ld_p.classList.add('long_desc');
+    div.appendChild(title_p);
+    div.appendChild(sd_p);
+    div.appendChild(ld_p);
 
-
-  div.classList.add('notification');
-
-  return div;
+    div.classList.add('notification');
+    return div;
 }
 
 
@@ -92,19 +90,19 @@ function notification_pretty_printer(notification) {
  * Authors: Ahmed H. Ismail.        *
  ************************************/
 function init_notification_list() {
-  //add on click event
-  $('#notifications_list').find('li:has(ul)')
-    .click( function(event) {
-      if (this == event.target) {
-        $(this).toggleClass('expanded');
-        $(this).children('ul').toggle('medium');
-      }
-      return false;
-    }).addClass('collapsed')
-    .children('ul').hide();
+    //add on click event
+    $('#notifications_list').find('li:has(ul)')
+      .click( function(event) {
+        if (this == event.target) {
+            $(this).toggleClass('expanded');
+            $(this).children('ul').toggle('medium');
+        }
+        return false;
+      }).addClass('collapsed')
+      .children('ul').hide();
 }
 
 $(document).ready(function() {
-  init_notification_list();
-  notifications_connection.trigger("notifications.pending"); // grab notifications
+    init_notification_list();
+    notifications_connection.trigger("notifications.pending"); // grab notifications
 });
