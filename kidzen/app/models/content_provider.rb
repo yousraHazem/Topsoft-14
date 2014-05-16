b8be02632a9cbc08285535cf41d0223a8fcbdccb
Nour2610 <nour.tarek2610@gmail.com>
2014-05-16 21:16:16 +0200

    Revert "including refactored code for sign in"
    This reverts commit 43eab34c4463101b2aa1e3b7a3b512c3fd431e51.
    
    Conflicts:
    	kidzen/app/models/content_provider.rb

--- kidzen/app/models/content_provider.rb
+++ kidzen/app/models/content_provider.rb
@@ -1,22 +1,2 @@
 class ContentProvider < ActiveRecord::Base
-  include TokenHelper
-
-  private
-    VALID_EMAIL_REGEX = /\A([a-z.\-_\d]+)@([a-z\-_\d]+(\.[a-z]+)+)\z/
-
-  # Creates the token.
-  # Authors: Ahmed H. Ismail , Nouran Tarek Attia
-    def create_remember_token
-      self.remember_token = digest(new_remember_token)
-    end
-
-  public
-    #Validations for content provider
-    before_create :create_remember_token
-    validates :username, presence: true, length: { minimum: 4 },
-      uniqueness: true
-    validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, 
-      uniqueness: true
-
-
 end
