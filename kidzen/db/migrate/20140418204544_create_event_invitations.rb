d41f098c6ab1bd4f444756c01211634548b5079e
Shary George Beshara <sharybeshara@gmail.com>
2014-05-18 09:58:58 +0100

    fixing

--- kidzen/db/migrate/20140418204544_create_event_invitations.rb
+++ kidzen/db/migrate/20140418204544_create_event_invitations.rb
@@ -1,10 +1,12 @@
 class CreateEventInvitations < ActiveRecord::Migration
   def change
   	create_table :event_invitations do |t|
+      t.boolean :accept
       t.integer :event_id
       t.integer :inviter_id
       t.integer :invited_id
       t.timestamps
     end
+    add_index(:event_invitations, [:inviter_id, :invited_id], unique: true)
   end
 end
